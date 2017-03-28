5baa79f6ef2166d9175fcb7605f8a6359eedd5ff
Ron Hashimoto <mail@h2so5.net>
2017-03-28 22:00:30 +0900

    synlink

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,11 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir -p /deplug/tools/bin
+mkdir -p /deplug/tools
 unzip -q -o /root/.electron/electron-*.zip -d /deplug/tools/electron
 
-case $ELECTRON_PLATFORM in
-  darwin ) ln -f -s ../electron/Electron.app/Contents/MacOS/Electron /deplug/tools/bin/electron ;;
-  linux  ) ln -f -s ../electron/electron /deplug/tools/bin/electron ;;
-  win32  ) echo "Please run 'MKLINK tools/bin/electron ../electron/Electron.exe'" ;;
-  *      ) echo "unknown platform";;
-esac
+if [ "$ELECTRON_PLATFORM" == "darwin" ]; then
+  ln -f -s ../electron/Electron.app/Contents/MacOS/Electron /deplug/tools/electron/electron
+fi
