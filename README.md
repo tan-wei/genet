a645369f8ab8b972f7cfcbb89453f5cc2c86b333
Ron Hashimoto <mail@h2so5.net>
2017-03-28 20:07:36 +0900

    bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # deplug
+
+```
+$ cd deplug
+$ docker build -t deplug-env .
+$ docker run --rm -v $(pwd):/deplug deplug-env /deplug/scripts/bootstrap.sh
+```
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+
+mkdir -p /deplug/tools/bin
+unzip -q -o /root/.electron/electron-*.zip -d /deplug/tools/electron
+
+case $ELECTRON_PLATFORM in
+  darwin ) ln -f -s ../electron/Electron.app/Contents/MacOS/Electron /deplug/tools/bin/electron ;;
+  linux  ) ln -f -s ../electron/electron /deplug/tools/bin/electron ;;
+  win32  ) echo "Please run 'MKLINK tools/bin/electron ../electron/Electron.exe'" ;;
+  *      ) echo "unknown platform";;
+esac
