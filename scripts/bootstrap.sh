38d48c3b514edcf17503198b4b43899010647114
Ron Hashimoto <mail@h2so5.net>
2017-03-31 14:50:20 +0900

    bootstrap

--- scripts/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p /deplug/tools
-unzip -q -o /root/.electron/electron-*.zip -d /deplug/tools/electron
-
-if [ "$ELECTRON_PLATFORM" == "darwin" ]; then
-  ln -f -s ../electron/Electron.app/Contents/MacOS/Electron /deplug/tools/electron/electron
-fi
