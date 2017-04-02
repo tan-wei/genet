4c1340078c370b67669082c929e37083cdd24ee6
Ron Hashimoto <mail@h2so5.net>
2017-04-02 22:24:24 +0900

    elements

--- parcels/window/deplug-builtin-main-window/test.js
+++ parcels/window/deplug-builtin-main-window/test.js
@@ -3,6 +3,4 @@ export default function() {
   console.log('aaa')
   jquery('head')
     .append(jquery(`<link rel="import" href="${__dirname}/elements.htm">`))
-  jquery('body')
-    .append('<say-hi name="Eric"></say-hi>')
 }
