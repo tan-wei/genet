ec5db89aa1a31b5110ab685283fee3322004b5b0
Ron Hashimoto <mail@h2so5.net>
2017-04-02 22:40:42 +0900

    element

--- deplug-core/components/window.js
+++ deplug-core/components/window.js
@@ -6,11 +6,12 @@ import path from 'path'
 
 export default class WindowComponent extends Component {
   async load () {
+    let head = jquery('head')
     const less = objpath.get(this.comp, 'window.less', '')
     if (less !== '') {
       const lessFile = path.join(this.rootDir, less)
       const style = await Theme.current.render(lessFile)
-      jquery('head').append(jquery('<style>').text(style.css))
+      head.append(jquery('<style>').text(style.css))
     }
 
     const main = objpath.get(this.comp, 'window.main', '')
@@ -18,6 +19,12 @@ export default class WindowComponent extends Component {
       const mainFile = path.join(this.rootDir, main)
       const func = await this.roll(mainFile)
       func({}, this.rootDir)
+    }
+
+    const elements = objpath.get(this.comp, 'window.elements', [])
+    for (let elem of elements) {
+      const elemFile = path.join(this.rootDir, elem)
+      head.append(jquery(`<link rel="import" href="${elemFile}">`))
     }
   }
 }
--- parcels/window/deplug-builtin-main-window/test.js
+++ parcels/window/deplug-builtin-main-window/test.js
@@ -1,6 +1,4 @@
 import jquery from 'jquery'
 export default function() {
   console.log('aaa')
-  jquery('head')
-    .append(jquery(`<link rel="import" href="${__dirname}/elements.htm">`))
 }
