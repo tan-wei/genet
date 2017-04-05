7e0857ded2a6594b66e0e67dfe0084e4a93f1b59
Ron Hashimoto <mail@h2so5.net>
2017-04-06 00:41:11 +0900

    repeat

--- parcels/window/deplug-builtin-main-window/index.js
+++ parcels/window/deplug-builtin-main-window/index.js
@@ -1,19 +1,29 @@
 import m from 'mithril'
 
-const User = {
-  list: [
-    {
-      firstName: "hello",
-      lastName: "world"
-    }
-  ]
-}
+const tabs = [
+  {
+    name: "Preferences"
+  },
+  {
+    name: "Live capture 1"
+  }
+]
 
 export default class Main {
   view() {
     return (
       <main>
-        <div id="tab-container"></div>
+        <div id="tab-container">
+          {
+            tabs.map((t, i) => {
+              return (
+                <a class="tab-label" index={i} isActive={ 1 === i }>
+                  { t.name }
+                </a>
+              )
+            })
+          }
+        </div>
         <webview class="tab-content" src="https://www.github.com/"></webview>
       </main>
     )
