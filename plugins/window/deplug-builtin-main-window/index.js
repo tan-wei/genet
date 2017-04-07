66d7a433c8685a531f33cb543a8c47c2b822f83e
Ron Hashimoto <mail@h2so5.net>
2017-04-08 00:19:47 +0900

    item

--- plugins/window/deplug-builtin-main-window/index.js
+++ plugins/window/deplug-builtin-main-window/index.js
@@ -4,65 +4,10 @@ import m from 'mithril'
 import jquery from 'jquery'
 import i18n from 'i18n4v'
 
-const tabs = []
-const loadedTabs = []
-let currentIndex = 0
-let idCounter = 0
-
-Channel.on('core:tab:open', (_, template) => {
-  let tab = Tab.getTemplate(template)
-  if (tab.singleton === true) {
-    let index = tabs.findIndex((t) => { return t.tab.template === template })
-    if (index >= 0) {
-      currentIndex = index
-      m.redraw()
-      return
-    }
-  }
-  tabs.push({
-    id: idCounter++,
-    tab: Object.assign({}, tab)
-  })
-  currentIndex = tabs.length - 1
-  m.redraw()
-})
-
-Channel.on('core:tab:set-name', (_, id, name) => {
-  let item = tabs.find((t) => { return t.id === id })
-  if (item) {
-    item.tab.name = name
-    m.redraw()
-  }
-})
-
-Channel.on('core:tab:focus', (_, id) => {
-  let index = tabs.findIndex((t) => { return t.id === id })
-  if (index >= 0) {
-    currentIndex = index
-    m.redraw()
-  }
-})
-
-Channel.on('core:tab:close', (_, id) => {
-  let index = tabs.findIndex((t) => { return t.id === id })
-  if (index >= 0) {
-    if (currentIndex > 0 && currentIndex === tabs.length - 1) {
-      currentIndex--
-    }
-    tabs.splice(index, 1)
-    m.redraw()
-  }
-})
-
-ipcRenderer.on('tab-deplug-loaded', (_, id) => {
-  loadedTabs[id] = true
-  m.redraw()
-})
-
 class WebView {
   oncreate(vnode) {
     let webview = vnode.dom
-    let item = tabs.find((t) => { return t.id === parseInt(vnode.attrs.id) })
+    let item = vnode.attrs.item
     if (item) {
       webview.addEventListener('dom-ready', () => {
         const argv = JSON.stringify(Argv)
@@ -85,10 +30,67 @@ class WebView {
   }
 }
 
-export default class Main {
+export default class TabView {
+  constructor() {
+    this.idCounter = 0
+    this.currentIndex = 0
+    this.loadedTabs = []
+    this.tabs = []
+
+    Channel.on('core:tab:open', (_, template) => {
+      let tab = Tab.getTemplate(template)
+      if (tab.singleton === true) {
+        let index = this.tabs.findIndex((t) => { return t.tab.template === template })
+        if (index >= 0) {
+          this.currentIndex = index
+          m.redraw()
+          return
+        }
+      }
+      this.tabs.push({
+        id: this.idCounter++,
+        tab: Object.assign({}, tab)
+      })
+      this.currentIndex = this.tabs.length - 1
+      m.redraw()
+    })
+
+    Channel.on('core:tab:set-name', (_, id, name) => {
+      let item = this.tabs.find((t) => { return t.id === id })
+      if (item) {
+        item.tab.name = name
+        m.redraw()
+      }
+    })
+
+    Channel.on('core:tab:focus', (_, id) => {
+      let index = this.tabs.findIndex((t) => { return t.id === id })
+      if (index >= 0) {
+        this.currentIndex = index
+        m.redraw()
+      }
+    })
+
+    Channel.on('core:tab:close', (_, id) => {
+      let index = this.tabs.findIndex((t) => { return t.id === id })
+      if (index >= 0) {
+        if (this.currentIndex > 0 && this.currentIndex === this.tabs.length - 1) {
+          this.currentIndex--
+        }
+        this.tabs.splice(index, 1)
+        m.redraw()
+      }
+    })
+
+    ipcRenderer.on('tab-deplug-loaded', (_, id) => {
+      this.loadedTabs[id] = true
+      m.redraw()
+    })
+  }
+
   activate(index) {
-    currentIndex = parseInt(index)
-    let content = jquery(`webview[index=${currentIndex}]`).get()[0]
+    this.currentIndex = parseInt(index)
+    let content = jquery(`webview[index=${this.currentIndex}]`).get()[0]
   }
 
   view() {
@@ -96,16 +98,16 @@ export default class Main {
       <main>
         <div id="tab-container">
           {
-            tabs.map((t, i) => {
+            this.tabs.map((t, i) => {
               return (
                 <a class="tab-label"
                   index={i}
-                  isActive={ currentIndex === i }
+                  isActive={ this.currentIndex === i }
                   onclick={m.withAttr('index', this.activate, this)}
                 >
                 <i class="fa fa-times"
                   onclick={() => {
-                    if (currentIndex === i) {
+                    if (this.currentIndex === i) {
                       Channel.emit('core:tab:close', t.id)
                     }
                   }}
@@ -130,13 +132,14 @@ export default class Main {
         </div>
         <div id="tab-mask"></div>
         {
-          tabs.map((t, i) => {
+          this.tabs.map((t, i) => {
             return m(WebView, {
               key: t.id,
               id: t.id,
               index: i,
-              isLoaded: (loadedTabs[t.id] === true),
-              isActive: currentIndex === i
+              item: t,
+              isLoaded: (this.loadedTabs[t.id] === true),
+              isActive: this.currentIndex === i
             })
           })
         }
