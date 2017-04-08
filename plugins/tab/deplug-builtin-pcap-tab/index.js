12c517a9d278360375ccfb2154b8ae7521e71875
Ron Hashimoto <mail@h2so5.net>
2017-04-08 23:13:59 +0900

    sess

--- plugins/tab/deplug-builtin-pcap-tab/index.js
+++ plugins/tab/deplug-builtin-pcap-tab/index.js
@@ -3,18 +3,7 @@ import { Pcap, SessionFactory } from 'plugkit'
 
 class ConfigView {
   constructor() {
-    this.frame = {frames: 0}
-    let factory = new SessionFactory()
-    factory.networkInterface = Pcap.devices[0].id
-    factory.create().then((s) => {
-      s.on('frame', (stat) => {
-        this.frame = stat
-        m.redraw()
-      })
-      s.startPcap()
-    }, (err) => {
-      console.log(err)
-    })
+
   }
 
   view(vnode) {
@@ -22,9 +11,6 @@ class ConfigView {
         <h1>Live capture</h1>
         <p>
           Permission: { Pcap.permission ? 'OK' : 'NG'}
-        </p>
-        <p>
-          Frames: { `${this.frame.frames}` }
         </p>
         <ul>
           {
@@ -43,13 +29,50 @@ class ConfigView {
   }
 }
 
-class PcapView {
+class FrameView {
+  constructor() {
+    this.frame = {frames: 0}
+    this.session = null
+  }
+
   view(vnode) {
-    return <h1><a href="#!/">Pcap Pcap</a></h1>
+    if (this.session === null && vnode.attrs.session !== null) {
+      this.session = vnode.attrs.session
+      this.session.on('frame', (stat) => {
+        this.frame = stat
+        m.redraw()
+      })
+    }
+    return <div>
+      {
+        (new Array(this.frame.frames)).fill().map((dev) => {
+          return <li>{ "dev.name" }</li>
+        })
+      }
+    </div>
+  }
+}
+
+class PcapView {
+  constructor() {
+    this.session = null
+    let factory = new SessionFactory()
+    factory.networkInterface = Pcap.devices[0].id
+    factory.create().then((s) => {
+      this.session = s
+      s.startPcap()
+      m.redraw()
+    }, (err) => {
+      console.log(err)
+    })
+  }
+
+  view(vnode) {
+    return m(FrameView, {session: this.session})
   }
 }
 
 export default {
-  '/': ConfigView,
-  '/pcap': PcapView
+  '/pcap': ConfigView,
+  '/': PcapView
 }
