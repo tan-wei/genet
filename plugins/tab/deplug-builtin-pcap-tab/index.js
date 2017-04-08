8cf089268e5788cd1c426a58bf468190e91ce2b2
Ron Hashimoto <mail@h2so5.net>
2017-04-08 21:26:42 +0900

    pcap

--- plugins/tab/deplug-builtin-pcap-tab/index.js
+++ plugins/tab/deplug-builtin-pcap-tab/index.js
@@ -1,18 +1,55 @@
 import m from 'mithril'
+import { Pcap, SessionFactory } from 'plugkit'
 
-class View {
+class ConfigView {
+  constructor() {
+    this.frame = {frames: 0}
+    let factory = new SessionFactory()
+    factory.networkInterface = Pcap.devices[0].id
+    factory.create().then((s) => {
+      s.on('frame', (stat) => {
+        this.frame = stat
+        m.redraw()
+      })
+      s.startPcap()
+    }, (err) => {
+      console.log(err)
+    })
+  }
+
   view(vnode) {
-    return <h1><a href="#!/xxxx">Pcap</a></h1>
+    return <div>
+        <h1>Live capture</h1>
+        <p>
+          Permission: { Pcap.permission ? 'OK' : 'NG'}
+        </p>
+        <p>
+          Frames: { `${this.frame.frames}` }
+        </p>
+        <ul>
+          {
+            Pcap.devices.map((dev) => {
+              return <li>{ dev.name }</li>
+            })
+          }
+        </ul>
+        <h1>Import local file</h1>
+        <input
+          type="button"
+          value="Choose pcap file..."
+          onclick={ ()=>{ location.hash = "!/pcap" } }
+        ></input>
+      </div>
   }
 }
 
-class View2 {
+class PcapView {
   view(vnode) {
     return <h1><a href="#!/">Pcap Pcap</a></h1>
   }
 }
 
 export default {
-  '/': View,
-  '/xxxx': View2
+  '/': ConfigView,
+  '/pcap': PcapView
 }
