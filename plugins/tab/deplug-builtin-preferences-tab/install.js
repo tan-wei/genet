c4d4118bba46b9688bd8585a8b00169f2341c70c
Ron Hashimoto <mail@h2so5.net>
2017-04-15 18:13:31 +0900

    loading

--- plugins/tab/deplug-builtin-preferences-tab/install.js
+++ plugins/tab/deplug-builtin-preferences-tab/install.js
@@ -5,27 +5,35 @@ import m from 'mithril'
 
 export default class InstallView {
   constructor() {
-    this.packages = [
-      {'name': 'Now Loading...'}
-    ]
+    this.packages = []
+    this.loading = false
+    this.error = false
   }
 
   oncreate() {
     let deplugin = new Deplugin()
     deplugin.cache().then((packages) => {
       this.packages = packages
+      this.loading = true
       m.redraw()
       return deplugin.search()
     }).then((packages) => {
       this.packages = packages
+      this.loading = false
+      m.redraw()
+    }).catch(() => {
+      this.loading = false
+      this.error = true
       m.redraw()
     })
   }
 
   view(vnode) {
     return [
-      <h1>Install Plugins </h1>
-      ,
+      <h1>Install Plugins </h1>,
+      <p style={{visibility: (this.loading || this.error) ? 'visible' : 'hidden'}}>
+      <i class={this.error ? 'fa fa-exclamation-triangle' : 'fa fa-refresh fa-spin'}>
+      </i>{ this.error ? ' Error' : ' Fetching package list...' }</p>,
       <div>
         {
           this.packages.map((pkg) => {
