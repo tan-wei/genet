5d6b8a0401b39873cd47a85cef0993e7e3c40a0e
Ron Hashimoto <mail@h2so5.net>
2017-04-17 08:12:14 +0900

    add linux-before-install.sh

---
+++ ci/linux-before-install.sh
@@ -0,0 +1,5 @@
+export CC="gcc-5"
+export CXX="g++-5"
+wget http://www.tcpdump.org/release/libpcap-1.7.4.tar.gz
+tar xzf libpcap-1.7.4.tar.gz
+(cd libpcap-1.7.4 && ./configure -q --enable-shared=no && make -j2 && sudo make install)
