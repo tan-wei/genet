394fb11b979d5bd8961e96073eb73db6b28fd7fb
Ron Hashimoto <mail@h2so5.net>
2017-04-16 16:06:12 +0900

    moment

--- plugins/tab/deplug-builtin-preferences-tab/install.js
+++ plugins/tab/deplug-builtin-preferences-tab/install.js
@@ -1,39 +1,51 @@
 import { Deplugin } from 'deplugin'
-import { Plugin } from 'deplug'
+import { Plugin, Config } from 'deplug'
 import PluginCard from './plugin-card'
 import m from 'mithril'
+import moment from 'moment'
 
 export default class InstallView {
   constructor() {
     this.packages = []
     this.loading = false
-    this.error = false
+    this.deplugin = new Deplugin(Config.deplug.version)
   }
 
   oncreate() {
-    let deplugin = new Deplugin()
-    deplugin.cache().then((packages) => {
+    this.deplugin.cache().then((packages) => {
       this.packages = packages
-      this.loading = true
       m.redraw()
-      return deplugin.search()
-    }).then((packages) => {
-      this.packages = packages
-      this.loading = false
-      m.redraw()
-    }).catch(() => {
-      this.loading = false
-      this.error = true
-      m.redraw()
+      if (moment(this.deplugin.lastUpdated)
+        .isBefore(moment().subtract(1, 'hours'))) {
+        this.update()
+      }
     })
   }
 
+  update() {
+    if (!this.loading) {
+      this.loading = true
+      this.deplugin.search().then((packages) => {
+        this.packages = packages
+        this.loading = false
+        m.redraw()
+      }).catch(() => {
+        this.loading = false
+        m.redraw()
+      })
+    }
+  }
+
   view(vnode) {
+    const lastUpdated = moment(this.deplugin.lastUpdated).fromNow()
     return [
       <h1>Install Plugins </h1>,
-      <p style={{visibility: (this.loading || this.error) ? 'visible' : 'hidden'}}>
-      <i class={this.error ? 'fa fa-exclamation-triangle' : 'fa fa-refresh fa-spin'}>
-      </i>{ this.error ? ' Error' : ' Fetching package list...' }</p>,
+      <p style={{display: this.loading ? 'block' : 'none'}}>
+      <i class="fa fa-refresh fa-spin"></i> Fetching package list...</p>,
+      <p style={{display: !this.loading ? 'block' : 'none'}}>
+      <a href="javascript:void(0)" onclick={()=>{ this.update() }}>
+      <i class="fa fa-check"></i>{` Last updated: ${lastUpdated}`}</a>
+      </p>,
       <div>
         {
           this.packages.map((pkg) => {
