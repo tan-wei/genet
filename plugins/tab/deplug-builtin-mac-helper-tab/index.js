02ab8aa1ebb6ea9a8b3b73417fe1a8a92795ac82
Ron Hashimoto <mail@h2so5.net>
2017-05-02 21:16:09 +0900

    helper page

--- plugins/tab/deplug-builtin-mac-helper-tab/index.js
+++ plugins/tab/deplug-builtin-mac-helper-tab/index.js
@@ -1,3 +1,5 @@
+import { GlobalChannel } from 'deplug'
+import { Pcap } from 'plugkit'
 import m from 'mithril'
 
 export default class View {
@@ -6,34 +8,49 @@ export default class View {
     <section>
       <h1>A helper tool needs to be installed</h1>
       <p>
-        You need the administration password to install it.
+        <b>TL;DR:</b> macOS does not allow normal users to access pcap devices by default.
+        Deplug provides a tiny helper tool to handle this problem.
+        Once it has been installed, we can start a live capture without running Deplug as root.
+      </p>
+      <p>
+        Press the follwing button to install the helper tool.
       </p>
       <input
         type="button"
-        value="Install Helper Tool..."
-        onclick={ ()=>{ require('deplug-helper')() } }
+        value={ Pcap.permission ? 'Successfully Installed' : "Install Helper Tool..." }
+        disabled={ Pcap.permission }
+        onclick={ (event)=>{
+          require('deplug-helper')()
+          if (Pcap.permission) {
+            GlobalChannel.emit('core:tab:open', 'Pcap')
+          }
+        } }
       ></input>
     </section>
     <section>
       <h2>How it works</h2>
       <p>
-        <ul>
-          <li>Creates a new group named ‘access_bpf’</li>
-          <li>Puts the current user into the group</li>
-        </ul>
-        <ul>
-          <li>as a previliged user on every startup</li>
+        The helper tool installer will do:
+        <ol>
+          <li>Create a new group ‘access_bpf’</li>
+          <li>Put the current user into the group</li>
+          <li>Register a startup script</li>
+        </ol>
+        The startup script runs the following commands as a previliged user every time.
+        <ol>
           <li>chgrp /dev/bpf*</li>
-        </ul>
+          <li>chmod g+rw /dev/bpf*</li>
+        </ol>
       </p>
     </section>
     <section>
-      <h2>How to uninstall the Helper Tool?</h2>
-      <p>
-        <ul>
-          <li>sudo dscl . -delete /Groups/access_bpf</li>
-        </ul>
-      </p>
+      <h2>Uninstall the helper tool</h2>
+      <ol>
+        <li>sudo launchctl unload /Library/LaunchDaemons/net.deplug.DeplugHelper.plist</li>
+        <li>sudo rm /Library/LaunchDaemons/net.deplug.DeplugHelper.plist</li>
+        <li>sudo rm /Library/PrivilegedHelperTools/net.deplug.DeplugHelper</li>
+        <li>sudo dscl . -delete /Groups/access_bpf</li>
+      </ol>
     </section>
     </main>
   }
