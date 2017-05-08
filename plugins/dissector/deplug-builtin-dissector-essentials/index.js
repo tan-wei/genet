c6e5a51958fceebd6721cbc6eb9d50688294dd04
Ron Hashimoto <mail@h2so5.net>
2017-05-08 09:37:23 +0900

    smaples

--- plugins/dissector/deplug-builtin-dissector-essentials/index.js
+++ plugins/dissector/deplug-builtin-dissector-essentials/index.js
@@ -8,7 +8,7 @@ export default {
           {link: 1, id: '<eth>', name: '<Ethernet>'}
         ],
         samples: [
-          { pcap: 'samples/eth.pcap' }
+          { pcap: 'samples/tcp-ipv4.pcap' }
         ]
       }
     },
@@ -16,13 +16,19 @@ export default {
       type: 'dissector',
       dissector: {
         main: 'build/Release/ipv4.node'
-      }
+      },
+      samples: [
+        { pcap: 'samples/tcp-ipv4.pcap' }
+      ]
     },
     {
       type: 'dissector',
       dissector: {
         main: 'build/Release/tcp.node'
-      }
+      },
+      samples: [
+        { pcap: 'samples/tcp-ipv4.pcap' }
+      ]
     },
     {
       type: 'stream-dissector',
