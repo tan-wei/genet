9544a5bbaa34390a76729ef02c8d3451196cca19
Ron Hashimoto <mail@h2so5.net>
2017-05-09 14:58:27 +0900

    remove gem

--- ci/linux-before-install.sh
+++ ci/linux-before-install.sh
@@ -3,5 +3,3 @@ export CXX="g++-5"
 wget http://www.tcpdump.org/release/libpcap-1.7.4.tar.gz
 tar xzf libpcap-1.7.4.tar.gz
 (cd libpcap-1.7.4 && ./configure -q --enable-shared=no && make -j2 && sudo make install)
-
-sudo gem install deb-s3
