eaf90eca554c306a9b11af2dcfabb3422a1ea206
Ron Hashimoto <mail@h2so5.net>
2017-05-13 18:18:12 +0900

    deb-s3

--- ci/linux-before-install.sh
+++ ci/linux-before-install.sh
@@ -3,3 +3,5 @@ export CXX="g++-5"
 wget http://www.tcpdump.org/release/libpcap-1.7.4.tar.gz
 tar xzf libpcap-1.7.4.tar.gz
 (cd libpcap-1.7.4 && ./configure -q --enable-shared=no && make -j2 && sudo make install)
+
+gem install --user-install deb-s3
