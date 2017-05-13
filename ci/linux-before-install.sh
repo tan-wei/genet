a725ad8effc26fb04b79c1125ca02ae0297c0038
Ron Hashimoto <mail@h2so5.net>
2017-05-13 18:23:33 +0900

    path

--- ci/linux-before-install.sh
+++ ci/linux-before-install.sh
@@ -4,4 +4,5 @@ wget http://www.tcpdump.org/release/libpcap-1.7.4.tar.gz
 tar xzf libpcap-1.7.4.tar.gz
 (cd libpcap-1.7.4 && ./configure -q --enable-shared=no && make -j2 && sudo make install)
 
+export PATH=/home/travis/.gem/ruby/2.2.0/bin:$PATH
 gem install --user-install deb-s3
