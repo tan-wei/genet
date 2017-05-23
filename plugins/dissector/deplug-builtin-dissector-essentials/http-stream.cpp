886402da0575408373c7d3ed7f7a8fa15c116f3a
Ron Hashimoto <mail@h2so5.net>
2017-05-23 09:16:30 +0900

    http

--- plugins/dissector/deplug-builtin-dissector-essentials/http-stream.cpp
+++ plugins/dissector/deplug-builtin-dissector-essentials/http-stream.cpp
@@ -5,6 +5,7 @@
 #include <plugkit/session_context.hpp>
 #include <plugkit/chunk.hpp>
 #include <plugkit/fmt.hpp>
+#include <unordered_set>
 
 using namespace plugkit;
 
@@ -12,37 +13,48 @@ class HTTPDissector final : public StreamDissector {
 public:
   class Worker final : public StreamDissector::Worker {
   public:
+    Worker(const SessionContext &ctx) : ctx(ctx) {
+      const auto array =
+        ctx.options["deplug-builtin-dissector-essentials"]["httpPorts"].array();
+      for (const auto& port : array) {
+        ports.insert(port.uint64Value());
+      }
+    }
+
     LayerPtr analyze(const ChunkConstPtr &chunk) override {
       Layer child(chunk->streamNs() + " http", "HTTP");
       const auto &layer = chunk->layer();
       uint16_t srcPort = layer->propertyFromId("src")->value().uint64Value();
       uint16_t dstPort = layer->propertyFromId("dst")->value().uint64Value();
-      if (srcPort != 80 && dstPort != 80) {
+      if (ports.find(srcPort) == ports.end() && ports.find(dstPort) == ports.end()) {
         return LayerPtr();
       }
       child.setPayload(chunk->payload());
       return std::make_shared<Layer>(std::move(child));
     }
+
+  private:
+    SessionContext ctx;
+    std::unordered_set<uint16_t> ports;
   };
 
 public:
+  HTTPDissector(const SessionContext &ctx) : ctx(ctx) {}
   StreamDissector::WorkerPtr createWorker() override {
-    return StreamDissector::WorkerPtr(new HTTPDissector::Worker());
+    return StreamDissector::WorkerPtr(new HTTPDissector::Worker(ctx));
   }
   std::vector<std::regex> namespaces() const override {
     return std::vector<std::regex>{std::regex("tcp$")};
   }
+
+private:
+  SessionContext ctx;
 };
 
 class HTTPDissectorFactory final : public StreamDissectorFactory {
 public:
   StreamDissectorPtr create(const SessionContext &ctx) const override {
-    const auto ports =
-      ctx.options["deplug-builtin-dissector-essentials"]["httpPorts"].array();
-    for (const auto& port : ports) {
-      printf("++ %d\n", static_cast<int>(port.uint64Value()));
-    }
-    return StreamDissectorPtr(new HTTPDissector());
+    return StreamDissectorPtr(new HTTPDissector(ctx));
   };
 };
 
