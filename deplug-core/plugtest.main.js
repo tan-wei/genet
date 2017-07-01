3365d056828e62a7dacff4922f0231bd0c0b601c
Ron Hashimoto <mail@h2so5.net>
2017-07-02 04:41:31 +0900

    assertion

--- deplug-core/plugtest.main.js
+++ deplug-core/plugtest.main.js
@@ -2,6 +2,8 @@ import DissectorComponent from './components/dissector'
 import Plugin from './plugin'
 import Session from './session'
 import StreamDissectorComponent from './components/stream-dissector'
+import assert from 'assert'
+import objpath from 'object-path'
 
 Plugin.listPlugins()
 .then((list) => {
@@ -22,11 +24,25 @@ Plugin.listPlugins()
   return Promise.all(tasks)
 })
 .then(() => Session.runSampleAnalysis())
-.then(() => {
+.then((list) => {
+  for (const frame of list) {
+    const { sourceId } = frame
+    const sample = Session.samples[sourceId]
+    if (sample.assert) {
+      for (const line of sample.assert) {
+        for (const key in line.properties) {
+          const value = objpath.get(frame.layerFromId(line.layer), key)
+          assert.deepEqual(value, line.properties[key])
+        }
+      }
+    }
+  }
+
   // eslint-disable-next-line no-process-exit
   process.exit(0)
 })
 .catch(() => {
+
   // eslint-disable-next-line no-process-exit
   process.exit(-1)
 })
