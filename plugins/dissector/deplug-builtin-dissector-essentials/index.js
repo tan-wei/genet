61dd803a4b0cef3a70da535f463b54ae69566e30
Ron Hashimoto <mail@h2so5.net>
2017-07-05 22:05:49 +0900

    items

--- plugins/dissector/deplug-builtin-dissector-essentials/index.js
+++ plugins/dissector/deplug-builtin-dissector-essentials/index.js
@@ -12,7 +12,21 @@ export default {
             pcap: 'samples/tcp-ipv4.pcap',
             assert: 'test/eth.json'
           }
-        ]
+        ],
+        items: {
+          eth: {
+            name: 'Ethernet'
+          },
+          'eth.src': {
+            name: 'Source'
+          },
+          'eth.dst': {
+            name: 'Destination'
+          },
+          'eth.ethType': {
+            name: 'EtherType'
+          }
+        }
       }
     },
     {
@@ -21,7 +35,57 @@ export default {
         main: 'build/Release/ipv4.node',
         samples: [
           { pcap: 'samples/tcp-ipv4.pcap' }
-        ]
+        ],
+        items: {
+          ipv4: {
+            name: 'IPv4'
+          },
+          'ipv4.version': {
+            name: 'Version'
+          },
+          'ipv4.hLen': {
+            name: 'Internet Header Length'
+          },
+          'ipv4.type': {
+            name: 'Type Of Service'
+          },
+          'ipv4.tLen': {
+            name: 'Total Length'
+          },
+          'ipv4.id': {
+            name: 'Identification'
+          },
+          'ipv4.flags': {
+            name: 'Flags'
+          },
+          'ipv4.flags.reserved': {
+            name: 'Reserved'
+          },
+          'ipv4.flags.dontFrag': {
+            name: 'Don\'t Fragment'
+          },
+          'ipv4.flags.moreFrag': {
+            name: 'More Fragments'
+          },
+          'ipv4.fragment': {
+            name: 'Fragment Offset'
+          },
+          'ipv4.ttl': {
+            name: 'TTL'
+          },
+          'ipv4.protocol': {
+            name: 'Protocol'
+          },
+          'ipv4.checksum': {
+            name: 'Header Checksum'
+          },
+          'ipv4.src': {
+            name: 'Source'
+          },
+          'ipv4.dst': {
+            name: 'Destination'
+          }
+        }
       }
     },
     {
@@ -30,13 +94,104 @@ export default {
         main: 'build/Release/udp.node',
         samples: [
           { pcap: 'samples/ntp.pcap' }
-        ]
+        ],
+        items: {
+          udp: {
+            name: 'UDP'
+          },
+          'udp.src': {
+            name: 'Source'
+          },
+          'udp.dst': {
+            name: 'Destination'
+          },
+          'udp.length': {
+            name: 'Length'
+          },
+          'udp.checksum': {
+            name: 'Checksum'
+          }
+        }
       }
     },
     {
       type: 'dissector',
       dissector: {
-        main: 'build/Release/tcp.node'
+        main: 'build/Release/tcp.node',
+        items: {
+          tcp: {
+            name: 'TCP'
+          },
+          'tcp.src': {
+            name: 'Source'
+          },
+          'tcp.dst': {
+            name: 'Destination'
+          },
+          'tcp.seq': {
+            name: 'Sequence number'
+          },
+          'tcp.ack': {
+            name: 'Acknowledgment number'
+          },
+          'tcp.dOffset': {
+            name: 'Data offset'
+          },
+          'tcp.flags': {
+            name: 'Flags'
+          },
+          'tcp.flags.ns': {
+            name: 'NS'
+          },
+          'tcp.flags.cwr': {
+            name: 'CWR'
+          },
+          'tcp.flags.ece': {
+            name: 'ECE'
+          },
+          'tcp.flags.urg': {
+            name: 'URG'
+          },
+          'tcp.flags.ack': {
+            name: 'ACK'
+          },
+          'tcp.flags.psh': {
+            name: 'PSH'
+          },
+          'tcp.flags.rst': {
+            name: 'RST'
+          },
+          'tcp.flags.syn': {
+            name: 'SYN'
+          },
+          'tcp.flags.fin': {
+            name: 'FIN'
+          },
+          'tcp.window': {
+            name: 'Window size'
+          },
+          'tcp.checksum': {
+            name: 'Checksum'
+          },
+          'tcp.urgent': {
+            name: 'Urgent pointer'
+          },
+          'tcp.options': {
+            name: 'Options'
+          },
+          'tcp.options.nop': {
+            name: 'NOP'
+          },
+          'tcp.options.timestam': {
+            name: 'Timestamps'
+          },
+          'tcp.options.timestam.mt': {
+            name: 'My timestamp'
+          },
+          'tcp.options.timestam.et': {
+            name: 'Echo reply timestamp'
+          }
+        }
       }
     },
     {
