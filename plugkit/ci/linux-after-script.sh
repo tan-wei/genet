388065cc7d92489918e423b6d8eeeca465c273df
Ron Hashimoto <mail@h2so5.net>
2017-07-16 12:07:45 +0900

    remove plugkit/ci

--- plugkit/ci/linux-after-script.sh
+++
@@ -1,1 +0,0 @@
-aws s3 cp build/Release/plugkit.node s3://deplug-build-junk/plugkit/linux/x64/$(jq .version package.json -r)/plugkit.node --quiet --acl public-read --cache-control "max-age=3600"
--- plugkit/ci/osx-before-install.sh
+++
@@ -1,7 +0,0 @@
-brew update
-brew install gpg jq yarn awscli
-nvm install 7.4.0
-
-export PATH=/usr/local/opt/gnupg/libexec/gpgbin:$PATH
-export CC=clang
-export CXX=clang++
