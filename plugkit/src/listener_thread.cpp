609d64dd3d472ad4600ea1d6a702d06580db4709
Ron Hashimoto <mail@h2so5.net>
2017-07-17 10:46:27 +0900

    process frames

--- plugkit/src/listener_thread.cpp
+++ plugkit/src/listener_thread.cpp
@@ -1,5 +1,8 @@
 #include "listener_thread.hpp"
 #include "listener.hpp"
+#include "frame_store.hpp"
+#include "frame_view.hpp"
+#include <array>
 
 namespace plugkit {
 
@@ -24,9 +27,38 @@ ListenerThread::~ListenerThread() {}
 
 void ListenerThread::enter() {}
 
-bool ListenerThread::loop() { return false; }
+bool ListenerThread::loop() {
+  std::thread::id id = std::this_thread::get_id();
+  std::array<const FrameView *, 128> views;
+  size_t size = d->store->dequeue(d->offset, views.size(), &views[0], id);
+  if (size == 0)
+    return false;
+
+  bool updated = false;
+  for (size_t i = 0; i < size; ++i) {
+    const auto &view = views[i];
+    if (d->listener->analyze(view)) {
+      const std::vector<Property *> &properties = d->listener->properties();
+      const std::vector<Chunk *> &chunks = d->listener->chunks();
+      if (!properties.empty() || !chunks.empty()) {
+        updated = true;
+      }
+    }
+  }
+
+  if (updated) {
+    d->callback();
+  }
+  d->offset += size;
+  return true;
+}
 
 void ListenerThread::exit() {}
 
-void ListenerThread::close() {}
+void ListenerThread::close() {
+  std::thread::id id = thread.get_id();
+  if (id != std::thread::id()) {
+    d->store->close(id);
+  }
+}
 }
