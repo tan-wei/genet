25abfeab3c3fc46d567dc5df17f039f80106631f
Ron Hashimoto <mail@h2so5.net>
2017-07-25 07:27:31 +0900

    add linux-before-deploy.sh

---
+++ ci/linux-before-deploy.sh
@@ -0,0 +1,3 @@
+export GEM_PATH=/home/travis/.gem/ruby/2.2.0
+export PATH=/home/travis/.gem/ruby/2.2.0/bin:$PATH
+gem install --user-install deb-s3
--- ci/linux-before-install.sh
+++ ci/linux-before-install.sh
@@ -3,7 +3,3 @@ export CXX="g++-5"
 wget http://www.tcpdump.org/release/libpcap-1.7.4.tar.gz
 tar xzf libpcap-1.7.4.tar.gz
 (cd libpcap-1.7.4 && ./configure -q --enable-shared=no && make -j2 && sudo make install)
-
-export GEM_PATH=/home/travis/.gem/ruby/2.2.0
-export PATH=/home/travis/.gem/ruby/2.2.0/bin:$PATH
-gem install --user-install deb-s3
