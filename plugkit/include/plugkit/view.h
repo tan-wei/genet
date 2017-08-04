60578058bbe316fbb003a24cb9612ab2a143ae14
Ron Hashimoto <mail@h2so5.net>
2017-08-04 22:36:59 +0900

    readBE

--- plugkit/include/plugkit/view.h
+++ plugkit/include/plugkit/view.h
@@ -21,12 +21,29 @@ size_t View_length(View view);
 View View_slice(View view, size_t offset, size_t length);
 
 uint8_t View_readUint8(View view, Error *err);
+int8_t View_readInt8(View view, Error *err);
+
+uint16_t View_readUint16BE(View view, Error *err);
+uint32_t View_readUint32BE(View view, Error *err);
+uint64_t View_readUint64BE(View view, Error *err);
+
+int16_t View_readInt16BE(View view, Error *err);
+int32_t View_readInt32BE(View view, Error *err);
+int64_t View_readInt64BE(View view, Error *err);
+
+float View_readFloat32BE(View view, Error *err);
+double View_readFloat64BE(View view, Error *err);
+
 uint16_t View_readUint16LE(View view, Error *err);
 uint32_t View_readUint32LE(View view, Error *err);
+uint64_t View_readUint64LE(View view, Error *err);
 
-int8_t View_readInt8(View view, Error *err);
 int16_t View_readInt16LE(View view, Error *err);
 int32_t View_readInt32LE(View view, Error *err);
+int64_t View_readInt64LE(View view, Error *err);
+
+float View_readFloat32LE(View view, Error *err);
+double View_readFloat64LE(View view, Error *err);
 }
 }
 
--- plugkit/src/view.cpp
+++ plugkit/src/view.cpp
@@ -5,7 +5,30 @@
 namespace plugkit {
 
 namespace {
-const Token outOfBoundError = Token_get("Out of bound");
+
+template <class T> T readLE(const View &view, Error *err) {
+  if (View_length(view) < sizeof(T)) {
+    if (err) {
+      static const Token outOfBoundError = Token_get("Out of bound");
+      err->type = outOfBoundError;
+    }
+    return T();
+  }
+  return *reinterpret_cast<const T *>(view.begin);
+}
+
+template <class T> T readBE(const View &view, Error *err) {
+  if (View_length(view) < sizeof(T)) {
+    if (err) {
+      static const Token outOfBoundError = Token_get("Out of bound");
+      err->type = outOfBoundError;
+    }
+    return T();
+  }
+  char data[sizeof(T)];
+  std::reverse_copy(view.begin, view.begin + sizeof(T), data);
+  return *reinterpret_cast<const T *>(data);
+}
 }
 
 size_t View_length(View view) { return view.end - view.begin; }
@@ -18,56 +41,74 @@ View View_slice(View view, size_t offset, size_t length) {
 }
 
 uint8_t View_readUint8(View view, Error *err) {
-  if (View_length(view) < sizeof(uint8_t)) {
-    if (err)
-      err->type = outOfBoundError;
-    return uint8_t();
-  }
-  return *reinterpret_cast<const uint8_t *>(view.begin);
-}
-
-uint16_t View_readUint16LE(View view, Error *err) {
-  if (View_length(view) < sizeof(uint16_t)) {
-    if (err)
-      err->type = outOfBoundError;
-    return uint16_t();
-  }
-  return *reinterpret_cast<const uint16_t *>(view.begin);
-}
-
-uint32_t View_readUint32LE(View view, Error *err) {
-  if (View_length(view) < sizeof(uint32_t)) {
-    if (err)
-      err->type = outOfBoundError;
-    return uint32_t();
-  }
-  return *reinterpret_cast<const uint32_t *>(view.begin);
+  return readLE<uint8_t>(view, err);
 }
 
 int8_t View_readInt8(View view, Error *err) {
-  if (View_length(view) < sizeof(int8_t)) {
-    if (err)
-      err->type = outOfBoundError;
-    return int8_t();
-  }
-  return *reinterpret_cast<const int8_t *>(view.begin);
+  return readLE<int8_t>(view, err);
+}
+
+uint16_t View_readUint16BE(View view, Error *err) {
+  return readBE<uint16_t>(view, err);
+}
+
+uint32_t View_readUint32BE(View view, Error *err) {
+  return readBE<uint32_t>(view, err);
+}
+
+uint64_t View_readUint64BE(View view, Error *err) {
+  return readBE<uint64_t>(view, err);
+}
+
+int16_t View_readInt16BE(View view, Error *err) {
+  return readBE<int16_t>(view, err);
+}
+
+int32_t View_readInt32BE(View view, Error *err) {
+  return readBE<int32_t>(view, err);
+}
+
+int64_t View_readInt64BE(View view, Error *err) {
+  return readBE<int64_t>(view, err);
+}
+
+float View_readFloat32BE(View view, Error *err) {
+  return readBE<float>(view, err);
+}
+
+double View_readFloat64BE(View view, Error *err) {
+  return readBE<double>(view, err);
+}
+
+uint16_t View_readUint16LE(View view, Error *err) {
+  return readLE<uint16_t>(view, err);
+}
+
+uint32_t View_readUint32LE(View view, Error *err) {
+  return readLE<uint32_t>(view, err);
+}
+
+uint64_t View_readUint64LE(View view, Error *err) {
+  return readLE<uint64_t>(view, err);
 }
 
 int16_t View_readInt16LE(View view, Error *err) {
-  if (View_length(view) < sizeof(int16_t)) {
-    if (err)
-      err->type = outOfBoundError;
-    return int16_t();
-  }
-  return *reinterpret_cast<const int16_t *>(view.begin);
+  return readLE<int16_t>(view, err);
 }
 
 int32_t View_readInt32LE(View view, Error *err) {
-  if (View_length(view) < sizeof(int32_t)) {
-    if (err)
-      err->type = outOfBoundError;
-    return int32_t();
-  }
-  return *reinterpret_cast<const int32_t *>(view.begin);
+  return readLE<int32_t>(view, err);
+}
+
+int64_t View_readInt64LE(View view, Error *err) {
+  return readLE<int64_t>(view, err);
+}
+
+float View_readFloat32LE(View view, Error *err) {
+  return readLE<float>(view, err);
+}
+
+double View_readFloat64LE(View view, Error *err) {
+  return readLE<double>(view, err);
 }
 }
