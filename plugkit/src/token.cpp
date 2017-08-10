a668ba92dcaaed9b65e9ddb91f512980fb992803
Ron Hashimoto <mail@h2so5.net>
2017-08-11 03:29:57 +0900

    gperf

--- plugkit/src/token.cpp
+++ plugkit/src/token.cpp
@@ -7,6 +7,9 @@
 namespace plugkit {
 
 namespace {
+#define register
+#include "token_hash.h"
+#undef register
 
 std::unordered_map<std::string, Token> map;
 std::unordered_map<Token, const char *> reverseMap;
@@ -17,13 +20,17 @@ Token Token_get(const char *str) {
   if (str == nullptr || str[0] == '\0') {
     return Token_null();
   }
+  size_t len = std::strlen(str);
+  if (in_word_set(str, len)) {
+    return hash(str, len) + 1;
+  }
   std::lock_guard<std::mutex> lock(mutex);
   const std::string &key = str;
   auto it = map.find(key);
   if (it != map.end()) {
     return it->second;
   }
-  Token id = map.size() + 1;
+  Token id = map.size() + 1 + MAX_HASH_VALUE + 1;
   map.insert(std::make_pair(key, id));
   char *data = new char[key.size() + 1]();
   key.copy(data, key.size());
@@ -36,6 +43,9 @@ Token Token_null() { return Token{0}; }
 const char *Token_string(Token token) {
   if (token == Token_null()) {
     return "";
+  }
+  if (token <= MAX_HASH_VALUE + 1) {
+    return wordlist[token - 1];
   }
   std::lock_guard<std::mutex> lock(mutex);
   auto it = reverseMap.find(token);
