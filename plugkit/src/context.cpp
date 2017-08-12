ee5218857afdbe5269b13dab4db884c8aa1fd9d0
Ron Hashimoto <mail@h2so5.net>
2017-08-12 18:12:05 +0900

    remove Context_alloc

--- plugkit/src/context.cpp
+++ plugkit/src/context.cpp
@@ -4,15 +4,6 @@
 
 namespace plugkit {
 
-void *Context_alloc(Context *ctx, size_t size) {
-  // zero initialized
-  return new char[size]();
-}
-
-void Context_dealloc(Context *ctx, void *ptr) {
-  delete[] static_cast<char *>(ptr);
-}
-
 const Variant *Context_options(Context *ctx) { return &ctx->options; }
 
 namespace {
