a2cffd753058453bb39df70eea9a64f55a8070fc
Ron Hashimoto <mail@h2so5.net>
2017-08-31 23:53:52 +0900

    ipv4-literal

--- plugins/dissector/dissector-essentials/ipv4-literal.js
+++ plugins/dissector/dissector-essentials/ipv4-literal.js
@@ -1,11 +1,19 @@
+import esprima from 'esprima'
 import estraverse from 'estraverse'
+import objpath from 'object-path'
 
 export default function(ast)
 {
   return estraverse.replace(ast, {
     enter: (node) => {
-      if (node.type === 'TemplateLiteral')
-        return {type: 'Literal', value: '"ssss"'}
+      if (node.type === 'TemplateLiteral') {
+        const value = objpath.get(node, 'quasis.0.value.raw', '')
+        const addr = value.split('.').map(v => parseInt(v))
+        if (addr.length === 4 && addr.every(v => v >= 0 && v < 256)) {
+          const tree = esprima.parse(`(new Uint8Array(${JSON.stringify(addr)}))`)
+          return tree.body[0].expression
+        }
+      }
     }
   })
 }
