2fd824c71a9b119c8543c590310e99550ca2d4a3
Ron Hashimoto <mail@h2so5.net>
2017-09-03 16:47:38 +0900

    Context_streamIdFromSlices

--- plugkit/include/plugkit/context.h
+++ plugkit/include/plugkit/context.h
@@ -2,6 +2,7 @@
 #define PLUGKIT_CONTEXT_H
 
 #include <stddef.h>
+#include <stdint.h>
 #include "export.h"
 
 PLUGKIT_NAMESPACE_BEGIN
@@ -9,6 +10,7 @@ PLUGKIT_NAMESPACE_BEGIN
 typedef struct Variant Variant;
 typedef struct Layer Layer;
 typedef struct Context Context;
+typedef struct Slice Slice;
 
 PLUGKIT_EXPORT void *Context_alloc(Context *ctx, size_t size);
 
@@ -21,6 +23,10 @@ PLUGKIT_EXPORT const Variant *Context_options(Context *ctx);
 ///
 /// @remarks This function can be used only in analyze().
 PLUGKIT_EXPORT Variant *Context_addStreamIdentifier(Context *ctx, Layer *layer);
+
+PLUGKIT_EXPORT uint32_t Context_streamIdFromSlices(Context *ctx,
+                                                   const Slice *begin,
+                                                   size_t size);
 
 PLUGKIT_NAMESPACE_END
 
