0e3bef0174b3b61963cf7c0573d4fe7c4300d98c
Ron Hashimoto <mail@h2so5.net>
2017-09-10 23:54:32 +0900

    stream-panel

--- plugins/panel/stream-panel/view.js
+++ plugins/panel/stream-panel/view.js
@@ -1,10 +1,29 @@
 import m from 'mithril'
 import moment from 'moment'
-import { Channel, Profile } from 'deplug'
+import { Channel } from 'deplug'
 
 export default class StreamView {
   constructor() {
+    this.session = null
+    Channel.on('core:frame:selected', (frames) => {
+      if (frames.length) {
+        const id = frames[0].attr('tcp.streamId')
+        if (id) {
+          if (this.session) {
+            this.session.setDisplayFilter('stream-panel', `tcp.streamId === ${id.value}`)
+          }
+        }
+      }
+      m.redraw()
+    })
 
+    Channel.on('core:pcap:session-created', (sess) => {
+      this.session = sess
+      this.session.on('filter', (stat) => {
+        console.log(stat)
+        m.redraw()
+      })
+    })
   }
 
   view(vnode) {
