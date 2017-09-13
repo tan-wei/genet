e3adf1bc508c75969cd1f53348e98d7ff20acb5a
Ron Hashimoto <mail@h2so5.net>
2017-09-14 08:47:32 +0900

    Revert "ipv4"
    This reverts commit 696bcaeedfec9a5205064abf26db1c6fc44f0be3.

--- plugins/dissector/dissector-essentials/ipv4-addr.js
+++ plugins/dissector/dissector-essentials/ipv4-addr.js
@@ -1,14 +1,12 @@
 import m from 'mithril'
 
 export default class IPv4Addr {
+  constructor() {
+
+  }
+
   view(vnode) {
     const value = vnode.attrs.prop.value
-    const addr = [
-      (value >> 24) & 0xff,
-      (value >> 16) & 0xff,
-      (value >> 8) & 0xff,
-      value & 0xff
-    ]
-    return <span>{addr[0]}.{addr[1]}.{addr[2]}.{addr[3]}</span>
+    return <span>{value[0]}.{value[1]}.{value[2]}.{value[3]}</span>
   }
 }
