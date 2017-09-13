bb80bdc5d274b54204dd9427431a132455f317fe
Ron Hashimoto <mail@h2so5.net>
2017-09-14 08:49:26 +0900

    remove IPv4Addr::constructor()

--- plugins/dissector/dissector-essentials/ipv4-addr.js
+++ plugins/dissector/dissector-essentials/ipv4-addr.js
@@ -1,10 +1,6 @@
 import m from 'mithril'
 
 export default class IPv4Addr {
-  constructor() {
-
-  }
-
   view(vnode) {
     const value = vnode.attrs.prop.value
     return <span>{value[0]}.{value[1]}.{value[2]}.{value[3]}</span>
