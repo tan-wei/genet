c6c3fade1e5d872751ecc80be9edbe9c97f16d8a
Ron Hashimoto <mail@h2so5.net>
2017-09-14 21:53:48 +0900

    iterator comparison

--- plugkit/filter.js
+++ plugkit/filter.js
@@ -28,6 +28,83 @@ function $_op(opcode, ...args) {
   } else if (args.length === 2) {
     const left = $_value(args[0])
     const right = $_value(args[1])
+
+    if (typeof left === 'object' && Symbol.iterator in left
+        && typeof right === 'object' && Symbol.iterator in right) {
+      const leftIt = left[Symbol.iterator]()
+      const rightIt = right[Symbol.iterator]()
+      switch (opcode) {
+        case '==':
+        case '===':
+        {
+          while (true) {
+            const leftValue = leftIt.next()
+            const rightValue = rightIt.next()
+            if (leftValue.value !== rightValue.value ||
+              leftValue.done !== rightValue.done) return false
+            if (leftValue.done && rightValue.done) return true
+          }
+        }
+        break
+        case '!=':
+        case '!==':
+        {
+          while (true) {
+            const leftValue = leftIt.next()
+            const rightValue = rightIt.next()
+            if (leftValue.value !== rightValue.value ||
+              leftValue.done !== rightValue.done) return true
+            if (leftValue.done && rightValue.done) return false
+          }
+        }
+        break
+        case '<':
+        {
+          while (true) {
+            const leftValue = leftIt.next()
+            const rightValue = rightIt.next()
+            if (leftValue.value < rightValue.value) return true
+            if (leftValue.value > rightValue.value) return false
+            if (leftValue.done || rightValue.done) return false
+          }
+        }
+        break
+        case '<=':
+        {
+          while (true) {
+            const leftValue = leftIt.next()
+            const rightValue = rightIt.next()
+            if (leftValue.value < rightValue.value) return true
+            if (leftValue.value > rightValue.value) return false
+            if (leftValue.done || rightValue.done) return true
+          }
+        }
+        break
+        case '>':
+        {
+          while (true) {
+            const leftValue = leftIt.next()
+            const rightValue = rightIt.next()
+            if (leftValue.value > rightValue.value) return true
+            if (leftValue.value < rightValue.value) return false
+            if (leftValue.done || rightValue.done) return false
+          }
+        }
+        break
+        case '>=':
+        {
+          while (true) {
+            const leftValue = leftIt.next()
+            const rightValue = rightIt.next()
+            if (leftValue.value > rightValue.value) return true
+            if (leftValue.value < rightValue.value) return false
+            if (leftValue.done || rightValue.done) return true
+          }
+        }
+        break
+      }
+    }
+
     switch (opcode) {
     case 'instanceof':
       return args[0] instanceof args[1]
