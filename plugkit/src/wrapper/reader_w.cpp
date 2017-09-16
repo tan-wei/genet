de32ef9058508475f070c15447f3883ed8670679
Ron Hashimoto <mail@h2so5.net>
2017-09-17 07:49:58 +0900

    ReaderWrapper::slice

--- plugkit/src/wrapper/reader_w.cpp
+++ plugkit/src/wrapper/reader_w.cpp
@@ -11,6 +11,8 @@ void ReaderWrapper::init(v8::Isolate *isolate, v8::Local<v8::Object> exports) {
   v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
   tpl->InstanceTemplate()->SetInternalFieldCount(1);
   tpl->SetClassName(Nan::New("Reader").ToLocalChecked());
+  SetPrototypeMethod(tpl, "slice", slice);
+  SetPrototypeMethod(tpl, "sliceAll", sliceAll);
   SetPrototypeMethod(tpl, "getUint8", getUint8);
   SetPrototypeMethod(tpl, "getInt8", getInt8);
   SetPrototypeMethod(tpl, "getUint16", getUint16);
@@ -31,6 +33,31 @@ NAN_METHOD(ReaderWrapper::New) {
   ReaderWrapper *obj = new ReaderWrapper();
   obj->Wrap(info.This());
   info.GetReturnValue().Set(info.This());
+}
+
+NAN_METHOD(ReaderWrapper::slice) {
+  ReaderWrapper *wrapper = ObjectWrap::Unwrap<ReaderWrapper>(info.Holder());
+  if (Reader *reader = &wrapper->reader) {
+    const Slice &slice =
+        Reader_slice(reader, info[0]->NumberValue(), info[1]->NumberValue());
+    size_t sliceLen = Slice_length(slice);
+    info.GetReturnValue().Set(v8::Uint8Array::New(
+        v8::ArrayBuffer::New(v8::Isolate::GetCurrent(),
+                             const_cast<char *>(slice.begin), sliceLen),
+        0, sliceLen));
+  }
+}
+
+NAN_METHOD(ReaderWrapper::sliceAll) {
+  ReaderWrapper *wrapper = ObjectWrap::Unwrap<ReaderWrapper>(info.Holder());
+  if (Reader *reader = &wrapper->reader) {
+    const Slice &slice = Reader_sliceAll(reader, info[0]->NumberValue());
+    size_t sliceLen = Slice_length(slice);
+    info.GetReturnValue().Set(v8::Uint8Array::New(
+        v8::ArrayBuffer::New(v8::Isolate::GetCurrent(),
+                             const_cast<char *>(slice.begin), sliceLen),
+        0, sliceLen));
+  }
 }
 
 NAN_METHOD(ReaderWrapper::getUint8) {
