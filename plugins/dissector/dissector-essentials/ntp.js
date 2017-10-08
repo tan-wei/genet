124e4a78e904f375b357873e5e716b2576f1f0ad
Ron Hashimoto <mail@h2so5.net>
2017-10-09 00:32:34 +0900

    ntp

--- plugins/dissector/dissector-essentials/ntp.js
+++ plugins/dissector/dissector-essentials/ntp.js
@@ -1,11 +1,17 @@
 import { Reader, Layer, Token } from 'plugkit'
 
 const ntpToken = Token.get('ntp')
+const srcToken = Token.get('.src')
+const dstToken = Token.get('.dst')
 
 export default class NTP {
   analyze(ctx, layer) {
     const child = layer.addLayer(ntpToken)
     child.confidence = Layer.ConfProbable
+
+    if (layer.attr(srcToken).value !== 123 || layer.attr(dstToken).value !== 123) {
+      child.confidence = Layer.ConfPossible
+    }
 
     const reader = new Reader()
   }
