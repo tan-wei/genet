16fb88461d8309d983564680c62d313bcc1caf3b
Ron Hashimoto <mail@h2so5.net>
2017-10-09 01:16:52 +0900

    liTable

--- plugins/dissector/dissector-essentials/ntp.js
+++ plugins/dissector/dissector-essentials/ntp.js
@@ -4,6 +4,13 @@ const ntpToken = Token.get('ntp')
 const srcToken = Token.get('.src')
 const dstToken = Token.get('.dst')
 
+const liTable = {
+  0: Token.get('noWarning'),
+  1: Token.get('sec61'),
+  2: Token.get('sec59'),
+  3: Token.get('unknown')
+}
+
 export default class NTP {
   analyze(ctx, layer) {
     const child = layer.addLayer(ntpToken)
