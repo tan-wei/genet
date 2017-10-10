80922015608dece2222ea1d8bd0045d241ff8a9c
Ron Hashimoto <mail@h2so5.net>
2017-10-10 10:12:08 +0900

    ntp

--- plugins/dissector/dissector-essentials/ntp.js
+++ plugins/dissector/dissector-essentials/ntp.js
@@ -7,6 +7,17 @@ const liTable = {
   3: Token.get('ntp.leapIndicator.unknown')
 }
 
+const modeTable = {
+  0: Token.get('ntp.mode.reserved'),
+  1: Token.get('ntp.mode.symmetricActive'),
+  2: Token.get('ntp.mode.symmetricPassive'),
+  3: Token.get('ntp.mode.client'),
+  4: Token.get('ntp.mode.server'),
+  5: Token.get('ntp.mode.broadcast'),
+  6: Token.get('ntp.mode.controlMessage'),
+  7: Token.get('ntp.mode.reservedForPrivate')
+}
+
 export default class NTP {
   analyze(ctx, layer) {
     const child = layer.addLayer('ntp')
@@ -18,15 +29,24 @@ export default class NTP {
 
     const reader = new Reader(layer.payloads[0].slices[0])
 
-    const first = reader.getUint8()
+    const head = reader.getUint8()
     const leapIndicator = child.addAttr('ntp.leapIndicator')
-    leapIndicator.value = first >> 6
+    leapIndicator.value = head >> 6
     leapIndicator.type = '@enum'
+    leapIndicator.range = reader.lastRange
+    leapIndicator.error = reader.lastError
 
     if (leapIndicator.value in liTable) {
       const attr = child.addAttr(liTable[leapIndicator.value])
       attr.value = true
+      attr.range = reader.lastRange
+      attr.error = reader.lastError
     }
+
+    const version = child.addAttr('ntp.version')
+    version.value = (head >> 3) & 0b111
+    version.range = reader.lastRange
+    version.error = reader.lastError
   }
 
   static get layerHints() {
