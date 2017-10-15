450e5520d2a5af0a3228d6bb826a6fbfccc22830
Ron Hashimoto <mail@h2so5.net>
2017-10-15 13:11:27 +0900

    add js docs

--- plugkit/js/layer.js
+++ plugkit/js/layer.js
@@ -1,6 +1,62 @@
-const Layer = {}
-Object.defineProperty(Layer, 'ConfDecodable', {value: 0})
-Object.defineProperty(Layer, 'ConfPossible',  {value: 1})
-Object.defineProperty(Layer, 'ConfProbable',  {value: 2})
-Object.defineProperty(Layer, 'ConfExact',     {value: 3})
-exports.Layer = Layer
+const layer = {}
+Object.defineProperty(layer, 'ConfDecodable', {value: 0})
+Object.defineProperty(layer, 'ConfPossible',  {value: 1})
+Object.defineProperty(layer, 'ConfProbable',  {value: 2})
+Object.defineProperty(layer, 'ConfExact',     {value: 3})
+exports.Layer = layer
+
+class Layer {
+
+  // @return Attr | null
+  attr(id) {}
+
+  // @return Layer
+  addLayer(id) {}
+
+  // @return Layer
+  addSubLayer(id) {}
+
+  // @return Paylaod
+  addPayload() {}
+
+  // @return Attr
+  addAttr(id) {}
+
+  addTag(id) {}
+
+  // @return Object
+  toJSON() {}
+
+  // @property String
+  get id() {}
+
+  // @property Integer
+  get worker() {}
+
+  // @property Integer
+  set worker(worker) {}
+
+  // @property Integer
+  get confidence() {}
+
+  // @property Integer
+  set confidence(confidence) {}
+
+  // @property Layer | null
+  get parent() {}
+
+  // @property Frame
+  get frame() {}
+
+  // @property Array<Layer>
+  get layers() {}
+
+  // @property Array<Attr>
+  get attrs() {}
+
+  // @property Array<Payload>
+  get payloads() {}
+
+  // @property Array<String>
+  get tags() {}
+}
--- plugkit/js/token.js
+++ plugkit/js/token.js
@@ -2,35 +2,41 @@ const nativeToken = exports.Token
 const tokenMap = new Map()
 const tokenReverseMap = new Map()
 
-function Token(strings, ...keys) {
-  return Token.get(String.raw(strings, ...keys))
+function token(strings, ...keys) {
+  return token_get(String.raw(strings, ...keys))
 }
 
-Object.defineProperty(Token, 'get', {
-  value: function(str) {
-    const value = tokenMap.get(str)
-    if (value !== void 0) {
-      return value
-    }
-    const id = nativeToken.get(str)
-    tokenMap.set(str, id)
-    return id
+function token_get(str) {
+  const value = tokenMap.get(str)
+  if (value !== void 0) {
+    return value
   }
-})
+  const id = nativeToken.get(str)
+  tokenMap.set(str, id)
+  return id
+}
 
-Object.defineProperty(Token, 'string', {
-  value: function(id) {
-    const value = tokenReverseMap.get(id)
-    if (value !== void 0) {
-      return value
-    }
-    const str = nativeToken.string(id)
-    if (str) {
-      tokenReverseMap.set(id, str)
-      return str
-    }
-    return ''
+function token_string(id) {
+  const value = tokenReverseMap.get(id)
+  if (value !== void 0) {
+    return value
   }
-})
+  const str = nativeToken.string(id)
+  if (str) {
+    tokenReverseMap.set(id, str)
+    return str
+  }
+  return ''
+}
 
-exports.Token = Token
+Object.defineProperty(token, 'get',    { value: token_get    })
+Object.defineProperty(token, 'string', { value: token_string })
+exports.Token = token
+
+class Token {
+  // @return Integer
+  get(str) {}
+
+  // @return String
+  string(id) {}
+}
