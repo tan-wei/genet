d6cfb61aa8fd7663a8ad8bea9b4c1aa6cb7ac1d5
Ron Hashimoto <mail@h2so5.net>
2017-10-23 09:44:07 +0900

    update docs

--- plugkit/js/payload.js
+++ plugkit/js/payload.js
@@ -2,24 +2,31 @@ class Payload {
 
   addSlice(slice) {}
 
-  // @return Attr
   addAttr(id) {}
 
+  // Find the first child `Attr` with the given `id` and return it.
+  //
+  // If no attribute is found, return `null`.
   // @return Attr | null
   attr(id) {}
 
   // @property Array<Uint8Array>
+  // Array of the slices.
   get slices() {}
 
   // @property Integer
+  // Length of the payload.
   get length() {}
 
   // @property Array<Attr>
+  // Array of the payload attrs.
   get attrs() {}
 
   // @property String
+  // Type of the payload.
   get type() {}
 
   // @property String
+  // Type of the payload.
   set type(type) {}
 }
