ea962096db582ef5647821bb3edb765b01b1698f
Ron Hashimoto <mail@h2so5.net>
2017-11-01 09:48:58 +0900

    add document-ready-promise

--- core/window.main.js
+++ core/window.main.js
@@ -1,10 +1,12 @@
 import { ipcRenderer, remote } from 'electron'
 import ThemeLoader from './theme-loader'
+import ready from 'document-ready-promise'
 
 export default async function (argv) {
   const loader = new ThemeLoader(`${__dirname}/theme.less`)
   await loader.load(`${__dirname}/window.less`, document.head)
   ipcRenderer.send('core:window:loaded', remote.getCurrentWindow().id)
+  await ready()
 }
 
 document.addEventListener('dragover', (event) => {
