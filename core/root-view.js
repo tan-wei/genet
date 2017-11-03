bb5a8ec1557aca1d4afba305b4c644bdcb3b7887
Ron Hashimoto <mail@h2so5.net>
2017-11-03 16:31:30 +0900

    nav

--- core/root-view.js
+++ core/root-view.js
@@ -2,6 +2,17 @@ import m from 'mithril'
 
 export default class RootView {
   view () {
-    return m('h1', ['hello'])
+    return [
+      m('nav', [
+        m('ul', [
+          m('li', ['Live Capture 1']),
+          m('li', ['Live Capture 2'])
+        ]),
+        m('ul', [
+          m('li', [m('i', { class: 'fa fa-cog' }), ' ', 'Preferences'])
+        ])
+      ]),
+      m('main', ['hello'])
+    ]
   }
 }
