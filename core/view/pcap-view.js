bdb20f0671ab27a7b661dd9b52c78e735b0e4af0
Ron Hashimoto <mail@h2so5.net>
2017-11-05 18:43:47 +0900

    dialog

--- core/view/pcap-view.js
+++ core/view/pcap-view.js
@@ -1,4 +1,15 @@
+import Dialog from '../lib/dialog'
 import m from 'mithril'
+
+class PcapDialog {
+  view (vnode) {
+    return m('h1', {
+      onclick: () => {
+        vnode.attrs.callback('hello')
+      },
+    }, ['hello'])
+  }
+}
 
 export default class PcapView {
   view () {
@@ -8,5 +19,10 @@ export default class PcapView {
         m('h1', ['Deplug'])
       ])
     ]
+  }
+
+  oncreate () {
+    const dialog = new Dialog(PcapDialog)
+    dialog.show().then((result) => console.log(result))
   }
 }
