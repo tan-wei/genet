159f1bfb870bc32713cb0e2b5a73830382ec58e6
Ron Hashimoto <mail@h2so5.net>
2017-11-07 10:49:17 +0900

    package

--- core/lib/package-manager.js
+++ core/lib/package-manager.js
@@ -1,3 +1,9 @@
+import env from './env'
+import glob from 'glob'
+import path from 'path'
+import { promisify } from 'util'
+
+const promiseGlob = promisify(glob)
 const fields = Symbol('fields')
 export default class PackageManager {
   constructor (config) {
@@ -5,6 +11,17 @@ export default class PackageManager {
       list: [],
       enabledComponents: new Set(),
     }
+  }
+
+  async update () {
+    const builtinPluginPattern =
+      path.join(env.builtinPackagePath, '/**/package.json')
+    const userPluginPattern =
+      path.join(env.userPackagePath, '/**/package.json')
+
+    const builtinPaths = await promiseGlob(builtinPluginPattern)
+    const userPaths = await promiseGlob(userPluginPattern)
+    console.log(builtinPaths, userPaths)
   }
 
   enableComponent (type) {
