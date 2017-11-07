5a9f6a36fbb52d2405e687f223b8b6dbf1651744
Ron Hashimoto <mail@h2so5.net>
2017-11-07 14:41:34 +0900

    package list

--- core/lib/package-manager.js
+++ core/lib/package-manager.js
@@ -1,19 +1,46 @@
+import { EventEmitter } from 'events'
 import env from './env'
 import glob from 'glob'
+import jsonfile from 'jsonfile'
 import path from 'path'
 import { promisify } from 'util'
 
+async function readFile (filePath) {
+    try {
+      const data = await jsonfile.readFileSync(filePath)
+      return {
+        data,
+        filePath,
+      }
+    } catch (err) {
+      return {}
+    }
+}
+
 const promiseGlob = promisify(glob)
 const fields = Symbol('fields')
-export default class PackageManager {
+export default class PackageManager extends EventEmitter {
   constructor (config) {
+    super()
     this[fields] = {
-      list: [],
+      config,
+      packages: new Map(),
       enabledComponents: new Set(),
+      updating: false,
     }
+    this.update()
+    config.watch('_.disabledPackages', () => {
+      this.update()
+    })
   }
 
   async update () {
+    const { config, updating, packages } = this[fields]
+    if (updating) {
+      return
+    }
+    this[fields].updating = true
+
     const builtinPluginPattern =
       path.join(env.builtinPackagePath, '/**/package.json')
     const userPluginPattern =
@@ -21,7 +48,21 @@ export default class PackageManager {
 
     const builtinPaths = await promiseGlob(builtinPluginPattern)
     const userPaths = await promiseGlob(userPluginPattern)
-    console.log(builtinPaths, userPaths)
+
+    const disabled = new Set()
+    for (const name of config.get('_.disabledPackages', [])) {
+      disabled.add(name)
+    }
+
+    const pkgs = (await Promise.all(
+      builtinPaths.map(readFile).concat(userPaths.map(readFile))))
+      .filter((pkg) => pkg.data)
+    for (const pkg of pkgs) {
+      packages.set(pkg.name, pkg.data)
+    }
+
+    this.emit('updated')
+    this[fields].updating = false
   }
 
   enableComponent (type) {
@@ -29,6 +70,10 @@ export default class PackageManager {
   }
 
   get list () {
-    return this[fields].list
+    const list = []
+    for (const pkg of this[fields].packages.values()) {
+      list.push(pkg)
+    }
+    return list
   }
 }
--- core/view/preference/package.js
+++ core/view/preference/package.js
@@ -1,6 +1,17 @@
 import m from 'mithril'
 export default class Plugin {
   view () {
-    return m('h1', ['Package'])
+    return [
+      m('h1', ['Package']),
+      m('section', deplug.packages.list.map((pkg) => m('h4', [
+          pkg.name, m('span', { class: 'schema-path' }, [pkg.version])
+        ])))
+    ]
+  }
+
+  oncreate () {
+    deplug.packages.on('updated', () => {
+      m.redraw()
+    })
   }
 }
