c0e7e59a2700d1037e0b1d34cfc51b60bd132eb6
Ron Hashimoto <mail@h2so5.net>
2017-11-10 00:37:19 +0900

    negatron-version-string: supports pre-release

--- scripts/negatron-version-string.js
+++ scripts/negatron-version-string.js
@@ -2,7 +2,9 @@
 
 const jsonfile = require('jsonfile')
 const path = require('path')
+const semver = require('semver')
 
 jsonfile.readFile(path.join(__dirname, '../package.json'), (err, obj) => {
-  console.log(obj.devDependencies.negatron)
+  const ver = semver.parse(obj.devDependencies.negatron)
+  console.log(`${ver.major}.${ver.minor}.${ver.patch}`)
 })
