aaab923992d6d538b6c02a7d83ea6f05fc6e65c1
Ron Hashimoto <mail@h2so5.net>
2017-11-11 00:40:16 +0900

    mac helper message

--- core/view/pcap/pcap-dialog.js
+++ core/view/pcap/pcap-dialog.js
@@ -5,8 +5,15 @@ import m from 'mithril'
 export default class PcapDialog {
   view (vnode) {
     const ifs = deplug.layout.get('_.pcap.interface')
+    if (!Pcap.permission) {
+      return m('div', [
+        m(PermissionMassage, {})
+      ])
+    }
     return m('div', [
-      m(PermissionMassage, {}),
+      m('p', [
+        m('i', { class: 'fa fa-check' }), ' Live capture is ready.'
+      ]),
       m('ul', [
         m('li', [
           m('select', { name: 'ifs' }, [
--- core/view/pcap/permission-message.js
+++ core/view/pcap/permission-message.js
@@ -1,22 +1,42 @@
 import { Pcap } from 'plugkit'
 import m from 'mithril'
 
+class MacHelper {
+  view () {
+    return m('p', [
+      m('section', [
+        m('h1', ['A helper tool needs to be installed']),
+        m('p', [
+          m('b', ['TL;DR:']),
+            ' macOS does not allow normal users to access pcap devices ' +
+            'by default.  Deplug provides a tiny helper tool to handle ' +
+            'this problem. Once it has been installed, we can start a ' +
+            'live capture without running Deplug as root.'
+        ]),
+        m('p', ['Press the follwing button to install the helper tool.']),
+        m('input', {
+          type: 'button',
+          value: Pcap.permission
+            ? '✔ Successfully Installed'
+            : 'Install',
+          disabled: Pcap.permission,
+          onclick: () => {
+            require('deplug-helper')()
+          },
+        })
+      ]),
+      m('p', [
+        m('a', { href: 'https://deplug.net/#/helper-tool' }, ['Read more...'])
+      ])
+    ])
+  }
+}
+
 export default class PermissionMassage {
   view () {
-    if (Pcap.permission) {
-      return m('p', [
-        m('i', { class: 'fa fa-check' }), ' Live capture is ready.'
-      ])
-    }
     switch (process.platform) {
       case 'darwin':
-        return m('p', [
-          m('i', { class: 'fa fa-exclamation-triangle' }),
-          ' Live capture is NOT ready.',
-          m('br'),
-          'Could not access /dev/bpf*. Please check if the ' +
-          'Deplug Helper Tool has been installed correctly.'
-        ])
+        return m(MacHelper, {})
       case 'linux':
         return m('p', [
           m('i', { class: 'fa fa-exclamation-triangle' }),
