d1a18413ecf9d892b75e635f21d7190640ec1331
Ron Hashimoto <mail@h2so5.net>
2017-11-13 19:56:56 +0900

    fix css

--- core/view/preference/general.js
+++ core/view/preference/general.js
@@ -7,14 +7,15 @@ export default class General {
       .filter(([id]) => id.startsWith('_.'))
     return [
       m('h1', ['General']),
-      config.map(([id, schema]) => m('section', [
-          m('h4', [schema.title, m('span', { class: 'schema-path' }, [id])]),
-          m(SchemaInput, {
-            id,
-            schema,
-          }),
-          m('p', { class: 'description' }, [schema.description])
-      ]))
+      m('section',
+        config.map(([id, schema]) => m('section', [
+            m('h4', [schema.title, m('span', { class: 'schema-path' }, [id])]),
+            m(SchemaInput, {
+              id,
+              schema,
+            }),
+            m('p', { class: 'description' }, [schema.description])
+        ])))
     ]
   }
 
