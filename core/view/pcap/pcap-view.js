8fdb99571bf64005a9d00efac6ecf979fa5d254f
Ron Hashimoto <mail@h2so5.net>
2017-11-25 21:39:07 +0900

    ResizeObserver

--- core/view/pcap/pcap-view.js
+++ core/view/pcap/pcap-view.js
@@ -3,11 +3,19 @@ import PcapDialog from './pcap-dialog'
 import m from 'mithril'
 
 export default class PcapView {
+  constructor () {
+    this.itemHeight = 80
+    this.stat = {
+      frames: 0,
+      queue: 0,
+    }
+  }
+
   view () {
     return [
       m('header', []),
       m('nav', [
-        m('div', { style: { height: '1000px' } })
+        m('div', { style: { height: `${this.stat.frames * this.itemHeight}px` } })
       ]),
       m('main', [
         m('h1', ['Deplug'])
@@ -16,13 +24,20 @@ export default class PcapView {
     ]
   }
 
-  oncreate () {
+  oncreate (vnode) {
+    const resizeObserver = new ResizeObserver(() => {
+      m.redraw()
+    })
+    resizeObserver.observe(vnode.dom)
+
     const dialog = new Dialog(PcapDialog)
     dialog.show({ cancelable: false }).then(async (ifs) => {
       const sess = await deplug.session.create(ifs)
       sess.startPcap()
       sess.on('frame', (stat) => {
         deplug.logger.debug(stat)
+        this.stat = stat
+        m.redraw()
       })
     })
   }
