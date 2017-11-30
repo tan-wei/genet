5f8b86d8420cd65c8ef47049ffca551c7f5f8cf7
Ron Hashimoto <mail@h2so5.net>
2017-11-30 15:24:10 +0900

    LayerItem

--- core/view/pcap/pcap-detail-view.js
+++ core/view/pcap/pcap-detail-view.js
@@ -1,4 +1,180 @@
 import m from 'mithril'
+import moment from 'moment'
+import objpath from 'object-path'
+
+function selectRange (range = []) {
+  deplug.action.emit('core:frame:range-selected', range)
+}
+
+const propSymbol = Symbol('prop')
+const orderSymbol = Symbol('order')
+function orderedProperties (obj) {
+  return Object.values(obj)
+    .sort((lhs, rhs) => lhs[orderSymbol] - rhs[orderSymbol])
+    .map((item) => item)
+}
+
+class PropertyItem {
+  view (vnode) {
+    const prop = vnode.attrs.property[propSymbol]
+    const children = orderedProperties(vnode.attrs.property)
+    let faClass = 'property'
+    if (children.length) {
+      faClass = 'property children'
+    }
+    const range = [
+      prop.range[0] + vnode.attrs.dataOffset,
+      prop.range[1] + vnode.attrs.dataOffset
+    ]
+    const { name } = deplug.session.token(prop.id)
+    const propRenderer = deplug.session.attrRenderer(prop.type) || 'p'
+    return m('li', {
+      'data-range': `${range[0]}:${range[1]}`,
+      onmouseover: () => selectRange(range),
+      onmouseout: () => selectRange(),
+    }, [
+      m('details', [
+        m('summary', { class: faClass }, [
+          m('label', [
+            m('i', { class: 'fa fa-circle-o' }, [' ']),
+            m('i', { class: 'fa fa-arrow-circle-right' }, [' ']),
+            m('i', { class: 'fa fa-arrow-circle-down' }, [' ']),
+            name, ': '
+          ]),
+
+          /*
+          M(propRenderer, {
+            prop,
+            layer: vnode.attrs.layer,
+          }),
+          */
+          m('label', {
+            class: 'error',
+            style: {
+              display: prop.error
+              ? 'inline'
+              : 'none',
+            },
+          }, [m('i', { class: 'fa fa-exclamation-triangle' }), ' ', prop.error])
+        ]),
+        m('ul', [
+          children.map((child) => m(PropertyItem, {
+              property: child,
+              layer: vnode.attrs.layer,
+            }))
+        ])
+      ])
+    ])
+  }
+}
+
+class BufferValueItem {
+  view (vnode) {
+    const maxLen = 6
+    const buffer = vnode.attrs.value
+    const hex = buffer.slice(0, maxLen).toString('hex') +
+      (buffer.length > maxLen
+        ? '...'
+        : '')
+    return m('span', ['[', buffer.length, ' bytes] 0x', hex])
+  }
+}
+
+class LayerItem {
+  view (vnode) {
+    const { layer } = vnode.attrs
+    let dataOffset = 0
+    let dataLength = 0
+    if (layer.parent) {
+      const [parentPayload] = layer.parent.payloads
+      const parentAddr = parentPayload.slices[0].addr
+      let rootPayload = parentPayload
+      for (let { parent } = layer.parent; parent; { parent } = parent) {
+        [rootPayload] = parent.payloads
+      }
+      const rootAddr = rootPayload.slices[0].addr
+      dataLength = parentPayload.slices[0].length
+      dataOffset = parentAddr[1] - rootAddr[1]
+    }
+    const range = [
+      dataOffset,
+      dataOffset + dataLength
+    ]
+    const layerId = layer.id
+    const { name } = deplug.session.token(layerId)
+    const propObject = { [layerId]: {} }
+    const properties = layer.attrs
+    for (let index = 0; index < properties.length; index += 1) {
+      const prop = properties[index]
+      let { id } = prop
+      if (id.startsWith('.')) {
+        id = layer.id + id
+      }
+      objpath.ensureExists(propObject, id, {})
+      const item = objpath.get(propObject, id)
+      item[propSymbol] = prop
+      item[orderSymbol] = index
+    }
+    return m('ul', [
+      m('li', {
+        'data-range': `${range[0]}:${range[1]}`,
+        onmouseover: () => selectRange(range),
+        onmouseout: () => selectRange(),
+      }, [
+        m('details', [
+          m('summary', {
+            class: 'layer children',
+            'data-layer': layer.tags.join(' '),
+          }, [
+            m('i', { class: 'fa fa-arrow-circle-right' }, [' ']),
+            m('i', { class: 'fa fa-arrow-circle-down' }, [' ']),
+            name, ' ',
+            m('span', {
+               style: {
+                display: layer.streamId > 0
+                  ? 'inline'
+                  : 'none',
+                },
+              },
+              [
+                m('i', { class: 'fa fa-exchange' }),
+                ' Stream #', layer.streamId]),
+                m('span', {
+                style: {
+                display: layer.confidence < 1.0
+                                  ? 'inline'
+                                  : 'none',
+                },
+              }, [
+                m('i', { class: 'fa fa-question-circle' }),
+                ' ',
+                layer.confidence * 100, '%'
+              ])
+          ]),
+          orderedProperties(propObject[layerId]).map((prop) => m(PropertyItem, {
+              property: prop,
+              layer,
+              dataOffset,
+            })),
+          m('a', ['Payloads']),
+          m('ul', [
+            layer.payloads.map(
+              (payload) => payload.slices.map(
+                (slice) => m('li', [
+                  ' ',
+                  m(BufferValueItem, { value: slice }),
+                  ' : ', payload.type, ' '])))
+          ])
+        ])
+      ]),
+      m('li', [
+        m('ul', [
+          layer.layers.map((child) => m(LayerItem, { layer: child }))
+        ])
+      ])
+    ])
+  }
+}
 
 export default class PcapDetailView {
   constructor () {
@@ -10,13 +186,39 @@ export default class PcapDetailView {
   }
 
   view () {
-    return [
-      m('h1', ['Deplug']),
-      m('p', [
-        (this.selectedFrame
-          ? this.selectedFrame.length
-          : '')
-      ])
-    ]
+    if (this.selectedFrame === null) {
+      return m('div', { class: 'layer-list-view' }, ['No frame selected'])
+    }
+    const frame = this.selectedFrame
+
+    const tsString =
+      moment(frame.timestamp).format('YYYY-MM-DDTHH:mm:ss.SSSZ')
+
+    let length = `${frame.rootLayer.payloads[0].slices[0].length}`
+    if (frame.length > frame.rootLayer.payloads[0].slices[0].length) {
+      length += ` (actual: ${frame.length})`
+    }
+
+    let children = frame.rootLayer.layers
+    const rootId = frame.rootLayer.id
+    if (rootId.startsWith('[')) {
+      children = frame.rootLayer.layers
+    }
+
+    return m('div', { class: 'layer-list-view' }, [
+      m('ul', [
+        m('li', [
+          m('i', { class: 'fa fa-circle-o' }),
+          m('label', [' Timestamp: ']),
+          m('span', [' ', tsString, ' '])
+        ]),
+        m('li', [
+          m('i', { class: 'fa fa-circle-o' }),
+          m('label', [' Length: ']),
+          m('span', [' ', length, ' '])
+        ])
+      ]),
+      children.map((layer) => m(LayerItem, { layer }))
+    ])
   }
 }
