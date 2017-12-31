29086755a17ac8731b31376b05299909b3090dae
Ron Hashimoto <mail@h2so5.net>
2017-12-31 16:50:40 +0900

    show loading message

--- core/view/window/stack.js
+++ core/view/window/stack.js
@@ -4,10 +4,16 @@ import m from 'mithril'
 export default class Stack {
   view (vnode) {
     const { parent } = vnode.attrs
-    return m('main', parent.tabs.map((tab) => m(WebView, {
+    return [
+      m('main', { class: 'loading' }, [
+        m('i', { class: 'fa fa-cog fa-spin fa-3x fa-fw' }),
+        m('h2', ['Now Loading...'])
+      ])
+    ].concat(
+      m('main', parent.tabs.map((tab) => m(WebView, {
       tab,
       key: tab.id,
       active: parent.activeTab === tab.id,
-    })))
+    }))))
   }
 }
