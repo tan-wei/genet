3e0f8eb4b3836a98668806234166416f68130a65
Ron Hashimoto <mail@h2so5.net>
2018-01-07 23:22:25 +0900

    fix RandomID table

--- plugkit/src/random_id.hpp
+++ plugkit/src/random_id.hpp
@@ -9,12 +9,14 @@ namespace plugkit {
 namespace RandomID {
 template <size_t length>
 std::string generate() {
+  static const char table[] =
+      "ABCDEFGHJKLMNPQRSTUVWXYZ123456789abcdefghijkmnopqrstuvwxyz";
   std::random_device rd;
   std::mt19937 mt(rd());
-  std::uniform_int_distribution<char> index('a', 'z');
+  std::uniform_int_distribution<int> index(0, sizeof(table) - 2);
   std::string str(length, ' ');
   for (size_t i = 0; i < length; ++i) {
-    str[i] = index(mt);
+    str[i] = table[index(mt)];
   }
   return str;
 }
