59201daa3a90bed4a5caa75bddc80be0283256de
Ron Hashimoto <mail@h2so5.net>
2018-01-08 15:19:39 +0900

    travis: use builtin g++

--- scripts/linux-before-install.sh
+++ scripts/linux-before-install.sh
@@ -1,5 +1,3 @@
-export CC="gcc-5"
-export CXX="g++-5"
 export CPATH=$CPATH:$HOME/usr/include
 export LIBRARY_PATH=$LIBRARY_PATH:$HOME/usr/lib
 wget http://www.tcpdump.org/release/libpcap-1.7.4.tar.gz
