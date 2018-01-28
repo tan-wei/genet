f7acd785d608136c64bcd176c797c1c77ff29fcd
Ron Hashimoto <mail@h2so5.net>
2018-01-28 21:27:25 +0900

    tcp-stream in rust (wip)

---
+++ package/tcp/crates/tcp-stream/src/lib.rs
@@ -0,0 +1,158 @@
+extern crate byteorder;
+extern crate libc;
+
+#[macro_use]
+extern crate plugkit;
+
+use std::collections::{HashMap, BTreeMap};
+use std::io::{Error, ErrorKind};
+use plugkit::layer::{Layer};
+use plugkit::context::Context;
+use plugkit::worker::Worker;
+use plugkit::variant::Value;
+use plugkit::token;
+
+#[derive(Debug)]
+struct Stream {
+  pub id: u32,
+  pub seq: i64,
+  pub len: usize,
+  offset: usize,
+  slices: BTreeMap<usize, &'static[u8]>
+}
+
+impl Stream {
+    fn new(id: u32) -> Stream {
+        return Stream {
+            id: id,
+            seq: -1,
+            len: 0,
+            offset: 0,
+            slices: BTreeMap::new()
+        }
+    }
+
+    fn put(&mut self, start: usize, data: &'static[u8]) {
+        if data.len() > 0 {
+            self.slices.insert(start, data);
+            let mut end = self.offset;
+            for (start, slice) in self.slices.iter_mut() {
+                if *start < end {
+
+                }
+                end = *start + slice.len();
+            }
+        }
+    }
+
+    fn fetch(&mut self) -> Box<[&'static[u8]]> {
+        let mut slices = Vec::<&'static[u8]>::new();
+        loop {
+            let pos;
+            if let Some((key, value)) = self.slices.iter().next() {
+                pos = *key;
+                if *key == self.offset {
+                    self.offset += value.len();
+                    slices.push(value);
+                } else {
+                    break
+                }
+            } else {
+                break
+            }
+            self.slices.remove(&pos);
+        }
+        slices.into_boxed_slice()
+    }
+}
+
+struct TCPWorker {
+    map: HashMap<(&'static[u8],&'static[u8],u32,u32), Stream>
+}
+
+impl TCPWorker {
+    fn new() -> TCPWorker {
+        return TCPWorker {
+            map: HashMap::new()
+        }
+    }
+}
+
+impl Worker for TCPWorker {
+    fn analyze(&mut self, ctx: &mut Context, layer: &mut Layer) -> Result<(), Error> {
+        let (slice, _) = {
+            let payload = layer
+                .payloads()
+                .next()
+                .ok_or(Error::new(ErrorKind::Other, "no payload"))?;
+            let slice = payload
+                .slices()
+                .next()
+                .ok_or(Error::new(ErrorKind::Other, "no slice"))?;
+
+            (slice, payload.range())
+        };
+
+        let stream_id = {
+            let parent = layer.parent().unwrap();
+            let parent_src : &[u8] = parent.attr(token::join(parent.id(), token!(".src"))).unwrap().get();
+            let parent_dst : &[u8] = parent.attr(token::join(parent.id(), token!(".dst"))).unwrap().get();
+            let src : u32 = layer.attr(token!("tcp.src")).unwrap().get();
+            let dst : u32 = layer.attr(token!("tcp.dst")).unwrap().get();
+            (parent_src, parent_dst, src, dst)
+        };
+
+        let id = (self.map.len() << 8) | layer.worker() as usize;
+        let stream = self.map.entry(stream_id).or_insert_with(|| Stream::new(id as u32));
+
+        let (seq, window, flags) = {
+            let seq : u32 = layer.attr(token!("tcp.seq")).unwrap().get();
+            let window : u16 = layer.attr(token!("tcp.window")).unwrap().get();
+            let flags : u8 = layer.attr(token!("tcp.flags")).unwrap().get();
+            (seq, window, flags)
+        };
+
+        let syn = (flags & (0x1 << 1)) != 0;
+        if syn {
+            if stream.seq < 0 {
+                let offset = stream.len;
+                stream.seq = seq as i64;
+                stream.put(offset, slice);
+                stream.len += slice.len();
+            }
+        } else if stream.seq >= 0 {
+            if slice.len() > 0 {
+                if seq >= stream.seq as u32 {
+                    let offset = stream.len + (seq - stream.seq as u32) as usize;
+                    stream.seq = seq as i64;
+                    stream.put(offset, slice);
+                    stream.len += slice.len();
+                } else if stream.seq - seq as i64 > window as i64 {
+                    let offset = stream.len + ((std::u32::MAX as u32 - stream.seq as u32) + seq) as usize;
+                    stream.seq = seq as i64;
+                    stream.put(offset, slice);
+                    stream.len += slice.len();
+                }
+            } else if (stream.seq + 1) % std::u32::MAX as i64 == seq as i64 {
+                stream.seq = seq as i64;
+            }
+        }
+
+        let payloads = stream.fetch();
+        if payloads.len() > 0 {
+            let chunk = layer.add_payload(ctx);
+            chunk.set_typ(token!("@stream"));
+            for s in &*payloads {
+                chunk.add_slice(s);
+            }
+        }
+
+        let child = layer.add_sublayer(ctx, token!("tcp-stream"));
+        child.add_tag(token!("tcp-stream"));
+        Ok(())
+    }
+}
+
+plugkit_module!({});
+plugkit_api_layer_hints!(token!("tcp"));
+plugkit_api_worker!(TCPWorker, TCPWorker::new());
