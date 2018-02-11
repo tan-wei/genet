5ac42c33ebe43c5a1ae07032a1a1bdfec1728d3a
Ron Hashimoto <mail@h2so5.net>
2018-02-12 02:12:30 +0900

    introduce fuse.js

--- core/view/pcap/filter-suggest.js
+++ core/view/pcap/filter-suggest.js
@@ -1,20 +1,31 @@
+import Fuse from 'fuse.js'
 import m from 'mithril'
 
 export default class FilterSuggest {
   view (vnode) {
-    const { enabled } = vnode.attrs
+    const { enabled, hint } = vnode.attrs
 
-    const items = Array.from(deplug.session.tokens.entries()).slice(0, 5)
+    let items = []
+    if (enabled) {
+      const source = Array.from(deplug.session.tokens.entries())
+        .map(([id, item]) => ({
+          id,
+          item,
+        }))
+      const fuse = new Fuse(source, { keys: ['id', 'item.name'] })
+      items = fuse.search(hint).slice(0, 6)
+    }
+
     return m('div', {
       class: 'suggest',
       style: {
-        display: enabled
+        display: items.length
           ? 'block'
           : 'none',
       },
     }, [
-      m('ul', items.map(([key, item]) => m('li', [
-        key,
+      m('ul', items.map(({ id, item }) => m('li', [
+        id,
         m('span', { class: 'description' }, [item.name])
       ])))
     ])
