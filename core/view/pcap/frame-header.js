26689cad567c3a176c17371d0ada809fedcb08c4
Ron Hashimoto <mail@h2so5.net>
2018-02-13 21:54:02 +0900

    FrameHeader

--- core/view/pcap/frame-header.js
+++ core/view/pcap/frame-header.js
@@ -1,14 +1,12 @@
 import m from 'mithril'
 
 export default class FrameHeader {
-  view () {
-    const labels = [
+  constructor () {
+    this.offset = 0
+    this.active = false
+    this.labels = [
       {
         name: 'Protocol',
-        width: 100,
-      },
-      {
-        name: 'Length',
         width: 100,
       },
       {
@@ -20,14 +18,76 @@ export default class FrameHeader {
         width: 100,
       },
       {
+        name: 'Length',
+        width: 100,
+      },
+      {
         name: 'Summary',
         width: 100,
       }
     ]
-    return m('div', { class: 'frame-header' },
-      labels.map((item) => m('span', { style: { width: `${item.width}px` } }, [
-        item.name,
-        m('div', { class: 'handle' })
-      ])))
+  }
+
+  applyWidth () {
+    this.active = false
+  }
+
+  view () {
+    const views = [m('div', {
+      class: 'base',
+      style: {
+        display: this.active
+          ? 'block'
+          : 'none',
+      },
+      onmouseup: () => {
+        this.applyWidth()
+      },
+      onmouseout: () => {
+        this.applyWidth()
+      },
+      onmousemove: (event) => {
+        const width = Math.max(10, event.offsetX - this.offset)
+        this.labels[this.index].width = width
+      },
+    })]
+    let offset = 0
+    for (let index = 0; index < this.labels.length; index += 1) {
+      const label = this.labels[index]
+      const last = index === this.labels.length - 1
+      offset += label.width
+      if (!last) {
+        views.push(m('div', {
+          class: 'handle',
+          'data-offset': offset - label.width,
+          style: {
+            left: `${offset}px`,
+            display: this.active
+              ? 'none'
+              : 'block',
+          },
+          onmousedown: (event) => {
+            this.index = index
+            this.active = true
+            this.offset =
+              Number.parseInt(event.target.getAttribute('data-offset'), 10)
+          },
+          onmouseup: () => {
+            this.active = false
+          },
+        }))
+      }
+      views.push(m('span',
+        {
+          style: {
+            width: last
+              ? 'auto'
+              : `${label.width}px`,
+          },
+        }, [
+          label.name
+        ]))
+    }
+    return m('div', { class: 'frame-header' }, views)
   }
 }
