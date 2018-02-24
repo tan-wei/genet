5e4788e94864b1268cdbef40a8417729b57442cc
Ron Hashimoto <mail@h2so5.net>
2018-02-25 00:58:50 +0900

    registry: rename cache to cache.txt

--- core/lib/schema-default.js
+++ core/lib/schema-default.js
@@ -48,7 +48,7 @@ export default {
         format: 'uri',
       },
       uniqueItems: true,
-      default: ['https://deplug.net/registry/cache'],
+      default: ['https://deplug.net/registry/cache.txt'],
     },
     '_.package.rustpath': {
       title: 'Rust Path',
