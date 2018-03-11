dbd11bfaa7dd610cb543e412fbf95b87fe76d5e7
Ron Hashimoto <mail@h2so5.net>
2018-03-11 17:22:56 +0900

    remove Attr c api

--- plugkit/src/attr.hpp
+++ plugkit/src/attr.hpp
@@ -37,69 +37,6 @@ private:
   } mData;
 };
 
-extern "C" {
-/// Return the ID of the attr.
-Token Attr_id(const Attr *attr);
-
-/// Return the range of the attr.
-Range Attr_range(const Attr *attr);
-
-/// Set the range of the attr.
-void Attr_setRange(Attr *attr, Range range);
-
-/// Return the type of the attr.
-Token Attr_type(const Attr *attr);
-
-/// Set the type of the attr.
-void Attr_setType(Attr *attr, Token type);
-
-/// Return the value associated with the attr.
-const Variant *Attr_value(const Attr *attr);
-
-/// Return the mutable value associated with the attr.
-Variant *Attr_valueRef(Attr *attr);
-
-/// Return the value associated with the attr as `bool`.
-bool Attr_bool(const Attr *attr);
-
-/// Set the value of the attr to the given `bool` value.
-void Attr_setBool(Attr *attr, bool value);
-
-/// Return the value associated with the attr as `int64_t`.
-int64_t Attr_int64(const Attr *attr);
-
-/// Set the value of the attr to the given `int64_t` value.
-void Attr_setInt64(Attr *attr, int64_t value);
-
-/// Return the value associated with the attr as `uint64_t`.
-uint64_t Attr_uint64(const Attr *attr);
-
-/// Set the value of the attr to the given `uint64_t` value.
-void Attr_setUint64(Attr *attr, uint64_t value);
-
-/// Return the value associated with the attr as `double`.
-double Attr_double(const Attr *attr);
-
-/// Set the value of the attr to the given `double` value.
-void Attr_setDouble(Attr *attr, double value);
-
-/// Return the value associated with the attr as a null-terminated string.
-const char *Attr_string(const Attr *attr);
-
-/// Set the value of the attr to the given string.
-///
-/// See [Variant_setString](diss-api-c.md#c-func-variant-setstring) for details.
-void Attr_setString(Attr *attr, const char *str, size_t length);
-
-/// Return the value associated with the attr as `Slice`.
-Slice Attr_slice(const Attr *attr);
-
-/// Set the value of the attr to the given `Slice` value.
-///
-/// See [Variant_setSlice](diss-api-c.md#c-func-variant-setslice) for details.
-void Attr_setSlice(Attr *attr, Slice slice);
-}
-
 } // namespace plugkit
 
 #endif
