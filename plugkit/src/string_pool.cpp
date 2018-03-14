fccb645250ca83c5091d3f8eaa13f896cb86a7fc
Ron Hashimoto <mail@h2so5.net>
2018-03-14 21:28:06 +0900

    add missing files

---
+++ plugkit/src/string_pool.cpp
@@ -0,0 +1,25 @@
+#include <cstring>
+#include "string_pool.hpp"
+
+namespace plugkit {
+const char *StringPool::get(const char *str, size_t length)
+{
+  if (length == 0) {
+    return "";
+  }
+  View view{str, length};
+  auto it = set.lower_bound(view);
+  if (it != set.end() && strncmp(view.data, it->data, length) == 0) {
+    return it->data;
+  }
+  auto ptr = std::make_shared<std::string>(str, length);
+  holder.push_back(ptr);
+  view.data = ptr->c_str();
+  set.insert(view);
+  return view.data;
+}
+
+bool StringPool::Comp::operator()(const View &lhs, const View &rhs) {
+    return strncmp(lhs.data, rhs.data, std::min(lhs.length, rhs.length)) < 0;
+}	
+}
---
+++ plugkit/src/string_pool.hpp
@@ -0,0 +1,28 @@
+#include <memory>
+#include <list>
+#include <set>
+
+#ifndef PLUGKIT_STRING_POOL_HPP
+#define PLUGKIT_STRING_POOL_HPP
+
+namespace plugkit {
+
+class StringPool {
+public:
+  const char *get(const char *str, size_t length);
+ 
+private:
+  struct View {
+     const char *data;
+     size_t length;
+  };
+  struct Comp {
+     bool operator()(const View &lhs, const View &rhs);
+  };
+  std::set<View, Comp> set;
+  std::list<std::shared_ptr<std::string>> holder;
+};
+
+}
+
+#endif
