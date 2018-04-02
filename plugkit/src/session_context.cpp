d35472d8fb942dcb2d7c50de467f02a6922fd726
Ron Hashimoto <mail@h2so5.net>
2018-04-02 23:25:30 +0900

    remove printf

--- plugkit/src/session_context.cpp
+++ plugkit/src/session_context.cpp
@@ -7,9 +7,7 @@ SharedContext *plugkit_in_create_shared_ctx();
 void plugkit_in_destroy_shared_ctx(SharedContext *ctx);
 }
 
-SessionContext::SessionContext() : shared(plugkit_in_create_shared_ctx()) {
-  printf("@@@ %p @@@", shared);
-}
+SessionContext::SessionContext() : shared(plugkit_in_create_shared_ctx()) {}
 
 SessionContext::~SessionContext() { plugkit_in_destroy_shared_ctx(shared); }
 
