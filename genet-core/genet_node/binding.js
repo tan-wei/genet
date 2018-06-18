eb9beb9ff413fef6c742fd4f5bb80d820aa8be23
Ron Hashimoto <mail@h2so5.net>
2018-06-18 21:30:44 +0900

    load-module

--- genet-core/genet_node/binding.js
+++ genet-core/genet_node/binding.js
@@ -1,1 +1,8 @@
-module.exports = require('bindings')('genet_node.node')
+const path = require('path')
+const modpath = path.join(__dirname, '../bin/genet_node/genet_node.node')
+  .replace(/\bapp\.asar\b/, 'app.asar.unpacked')
+try {
+  module.exports = require(modpath)
+} catch (err) {
+  module.exports = require('bindings')('genet_node.node')
+}
