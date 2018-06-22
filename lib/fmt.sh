370e6cc8c50123603849960e09f8f97ca8dc4d8e
Ron Hashimoto <mail@h2so5.net>
2018-06-22 22:23:08 +0900

    genet-abi

--- lib/fmt.sh
+++ lib/fmt.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-(cd genet_abi && cargo +nightly fmt)
+(cd genet-abi && cargo +nightly fmt)
 (cd genet-kernel && cargo +nightly fmt)
 (cd genet-sdk && cargo +nightly fmt)
 (cd genet-node && clang-format -i src/*.*)
--- lib/test.sh
+++ lib/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-(cd genet_abi && cargo test && cargo test --release)
+(cd genet-abi && cargo test && cargo test --release)
 (cd genet-kernel && cargo test && cargo test --release)
 (cd genet-sdk && cargo test && cargo test --release)
 #(cd genet-node && npm i && npm test)
