0be5c4f369464c68a0894be349d6c8a29dcf69a6
Ron Hashimoto <mail@h2so5.net>
2018-06-25 23:06:28 +0900

    pcap

--- package/pcap/crates/pcap-reader/src/lib.rs
+++ package/pcap/crates/pcap-reader/src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate genet_sdk;
 extern crate genet_abi;
+extern crate pcap;
 
 #[macro_use]
 extern crate lazy_static;
@@ -18,27 +19,16 @@ use genet_sdk::{
     ptr::Ptr,
     result::Result,
 };
+use pcap::Header;
 
 use std::iter;
-use std::process::{Command, Child, Stdio};
+use std::io::{Read, BufRead, BufReader};
+use std::process::{Command, Child, Stdio, ChildStdout};
 
 #[derive(Deserialize)]
 struct Arg {
     cmd: String,
     args: Vec<String>,
-}
-
-pub fn tcp_ipv4_pcap() -> &'static [u8] {
-    &[
-        0xd4, 0xc3, 0xb2, 0xa1, 0x02, 0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x04, 0x00, 0x01, 0x00, 0x00, 0x00, 0x33, 0xf6, 0x7e, 0x58, 0x88, 0x65,
-        0x0d, 0x00, 0x42, 0x00, 0x00, 0x00, 0x42, 0x00, 0x00, 0x00, 0xac, 0xbc, 0x32, 0xbc, 0x2a,
-        0x87, 0x80, 0x13, 0x82, 0x62, 0xa2, 0x45, 0x08, 0x00, 0x45, 0x00, 0x00, 0x34, 0x69, 0xaf,
-        0x40, 0x00, 0x31, 0x06, 0x01, 0xf7, 0xca, 0xe8, 0xee, 0x28, 0xc0, 0xa8, 0x64, 0x64, 0x00,
-        0x50, 0xc4, 0x27, 0x22, 0xdd, 0xb1, 0xc0, 0x63, 0x6a, 0x47, 0x9b, 0x80, 0x10, 0x00, 0x72,
-        0xf7, 0x6c, 0x00, 0x00, 0x01, 0x01, 0x08, 0x0a, 0xf9, 0x28, 0x89, 0x4f, 0x61, 0x8f, 0x78,
-        0x9d,
-    ]
 }
 
 #[derive(Clone)]
@@ -52,7 +42,8 @@ impl Reader for PcapReader {
                            .stdout(Stdio::piped())
                            .spawn()
                            .expect("failed to execute child");
-        Box::new(PcapReaderWorker { child })
+        let reader = BufReader::new(child.stdout.take().unwrap());
+        Box::new(PcapReaderWorker { child, reader })
     }
 
     fn id(&self) -> &str {
@@ -61,17 +52,25 @@ impl Reader for PcapReader {
 }
 
 struct PcapReaderWorker {
-	child: Child
+	child: Child,
+    reader: BufReader<ChildStdout>
 }
 
 impl ReaderWorker for PcapReaderWorker {
     fn read(&mut self) -> Result<Vec<Layer>> {
+        let mut header = String::new();
+        let _ = self.reader.read_line(&mut header);
+        let header: Header = serde_json::from_str(&header).unwrap();
+        let mut payload = vec![0u8; header.datalen as usize];
+        self.reader.read_exact(&mut payload);
+        /*
     	self.child.stdout.as_mut().unwrap();
         let layers = iter::repeat(())
             .take(100)
             .map(|_| Layer::new(&ETH_CLASS, tcp_ipv4_pcap()))
             .collect();
-        Ok(layers)
+        */
+        Ok(vec![])
     }
 }
 
