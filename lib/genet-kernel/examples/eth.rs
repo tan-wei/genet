2b339399e2694deb038144864c57895eaa9a2ff3
Ron Hashimoto <mail@h2so5.net>
2018-06-27 10:39:53 +0900

    eth

--- lib/genet-kernel/examples/eth.rs
+++ lib/genet-kernel/examples/eth.rs
@@ -5,6 +5,9 @@ extern crate genet_abi;
 #[macro_use]
 extern crate lazy_static;
 
+#[macro_use]
+extern crate maplit;
+
 use genet_sdk::{
     attr::{Attr, AttrBuilder, AttrClass},
     context::Context,
@@ -14,6 +17,8 @@ use genet_sdk::{
     ptr::Ptr,
     result::Result,
 };
+use std::collections::HashMap;
+use std::io::{Error, ErrorKind};
 
 struct EthWorker {}
 
@@ -29,7 +34,14 @@ impl Worker for EthWorker {
             } else {
                 layer.add_attr(typ_attr);
             }
-            layer.add_payload(&parent.data()[14..], token!());
+            if let Some(attr) = TYPE_MAP.get(&len) {
+                layer.add_attr(Attr::new(attr, 12..14));
+            }
+            let payload = &parent
+                .data()
+                .get(14..)
+                .ok_or_else(|| Error::new(ErrorKind::Other, "out of bounds"))?;
+            layer.add_payload(payload, token!());
             Ok(Status::Done(vec![layer]))
         } else {
             Ok(Status::Skip)
@@ -58,19 +70,27 @@ lazy_static! {
         .header(Attr::new(&DST_ATTR, 6..12))
         .build();
     static ref SRC_ATTR: Ptr<AttrClass> = AttrBuilder::new(token!("eth.src"))
-        .typ(token!("eth:mac"))
+        .typ(token!("@eth:mac"))
         .decoder(decoder::Slice())
         .build();
     static ref DST_ATTR: Ptr<AttrClass> = AttrBuilder::new(token!("eth.dst"))
-        .typ(token!("eth:mac"))
+        .typ(token!("@eth:mac"))
         .decoder(decoder::Slice())
         .build();
     static ref LEN_ATTR: Ptr<AttrClass> = AttrBuilder::new(token!("eth.len"))
         .decoder(decoder::UInt16BE())
         .build();
     static ref TYPE_ATTR: Ptr<AttrClass> = AttrBuilder::new(token!("eth.type"))
+        .typ(token!("@enum"))
         .decoder(decoder::UInt16BE())
         .build();
+    static ref TYPE_MAP: HashMap<u64, Ptr<AttrClass>> = hashmap!{
+        0x0800 => AttrBuilder::new(token!("eth.type.ipv4")).build(),
+        0x0806 => AttrBuilder::new(token!("eth.type.arp")).build(),
+        0x0842 => AttrBuilder::new(token!("eth.type.wol")).build(),
+        0x86DD => AttrBuilder::new(token!("eth.type.ipv6")).build(),
+        0x888E => AttrBuilder::new(token!("eth.type.eap")).build(),
+    };
 }
 
 genet_dissectors!(EthDissector {});
