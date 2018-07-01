e3a8435be88593588023c26a70300a70383137ba
Ron Hashimoto <mail@h2so5.net>
2018-07-01 22:24:38 +0900

    package

--- lib/fmt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-(cd genet-abi && cargo +nightly fmt)
-(cd genet-kernel && cargo +nightly fmt)
-(cd genet-sdk && cargo +nightly fmt)
-(cd genet-node && clang-format -i src/*.*)
---
+++ package/http/summary.js
@@ -0,0 +1,26 @@
+const m = require('mithril')
+class Summary {
+  view (vnode) {
+    const { layer } = vnode.attrs
+    const value = ['']
+    for (const attr of layer.attrs()) {
+      switch (attr.id) {
+        case 'http.version':
+          value.push(`HTTP/${attr.value.toPrecision(2)}`)
+          break
+        case 'http.path':
+          value.push(` ${attr.value}`)
+          break
+        default:
+          if (attr.id.startsWith('http.method.')) {
+            value.push(` ${deplug.session.tokenName(attr.id)}`)
+          } else if (attr.id.startsWith('http.status.')) {
+            const code = layer.attr('http.status').value
+            value.push(` ${code} ${deplug.session.tokenName(attr.id)}`)
+          }
+      }
+    }
+    return m('span', value)
+  }
+}
+module.exports = Summary
