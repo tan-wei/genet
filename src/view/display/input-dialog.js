41c76b609d7873feb253ac3a196ee109d74726dd
Ron Hashimoto <mail@h2so5.net>
2018-07-07 13:32:50 +0900

    input

--- src/view/display/input-dialog.js
+++ src/view/display/input-dialog.js
@@ -1,14 +1,43 @@
-import TabView from './tab-view'
+import PanelView from './panel-view'
 import m from 'mithril'
 
 export default class InputDialog {
+  constructor () {
+    this.input = ''
+  }
+
+  update (vnode) {
+    this.input = vnode.dom.querySelector('[name=input-id]').value
+  }
+
   view (vnode) {
     const { callback } = vnode.attrs
     const panels = genet.workspace.panelLayout['dialog:input'] || []
-    return m('div', { class: 'tool-view' },
-      panels.map((group) => m(TabView, {
-        tabs: group,
-        attrs: { callback },
-      })))
+    const layout = [].concat.apply([], panels).map((tab) => genet.workspace.panel(tab))
+      .filter((panel) => typeof panel !== 'undefined')
+    if (this.input === '' && layout.length > 0) {
+      this.input = layout[0].id
+    }
+    return m('div', { class: 'tool-view' }, [
+      m('ul', [
+        m('li', [
+          m('select', {
+            name: 'input-id',
+            onchange: () => this.update(vnode),
+          }, layout.map((panel) =>
+            m('option', { value: panel.id }, [panel.name])))
+        ])
+      ]),
+      m('div',
+        layout.map((panel) => m('div', {
+          style: {
+            display: panel.id === this.input
+              ? 'block'
+              : 'none',
+          },
+        }, [
+          m(PanelView, Object.assign(panel, { attrs: { callback } }))
+        ]))
+      )])
   }
 }
