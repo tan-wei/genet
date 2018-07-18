c003551c5c707aeabda9596376c4f9185e59574e
Ron Hashimoto <mail@h2so5.net>
2018-07-18 21:45:42 +0900

    deb/rpm

--- scripts/deb.sh
+++ scripts/deb.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 GENET_VER=$(jq -r '.version' package.json)
-cp -r debian/. out/.debian
-sed -e "s/{{GENET_VERSION}}/$GENET_VER/g" debian/DEBIAN/control > out/.debian/DEBIAN/control
-mkdir -p out/.debian/usr/share/icons/hicolor/256x256/apps
-cp images/genet.png out/.debian/usr/share/icons/hicolor/256x256/apps
-cp -r out/genet-linux-x64/. out/.debian/usr/share/genet
-mv out/.debian/usr/share/genet/genet out/.debian/usr/share/genet/genet
-chrpath -r /usr/share/genet out/.debian/usr/share/genet/genet
-(cd out/.debian && fakeroot dpkg-deb --build . ../genet-linux-amd64.deb)
+cp -r debian/. out/src/.debian
+sed -e "s/{{GENET_VERSION}}/$GENET_VER/g" debian/DEBIAN/control > out/src/.debian/DEBIAN/control
+mkdir -p out/src/.debian/usr/share/icons/hicolor/256x256/apps
+cp images/genet.png out/src/.debian/usr/share/icons/hicolor/256x256/apps
+cp -r out/dist/genet-linux-x64/. out/src/.debian/usr/share/genet
+mv out/src/.debian/usr/share/genet/genet out/src/.debian/usr/share/genet/genet
+chrpath -r /usr/share/genet out/src/.debian/usr/share/genet/genet
+(cd out/src/.debian && fakeroot dpkg-deb --build . ../../genet-linux-amd64.deb)
--- scripts/rpm.sh
+++ scripts/rpm.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 GENET_VER=$(jq -r '.version | gsub("-";"~")' package.json)
-mkdir -p out/.rpm
-(cd out/.rpm && mkdir -p SOURCES BUILD RPMS SRPMS)
-sed -e "s/{{GENET_VERSION}}/$GENET_VER/g" genet.rpm.spec > out/.rpm/genet.rpm.spec
-cp -r debian/usr out/.rpm/BUILD
-mkdir -p out/.rpm/BUILD/usr/share/icons/hicolor/256x256/apps
-cp images/genet.png out/.rpm/BUILD/usr/share/icons/hicolor/256x256/apps
-cp -r out/genet-linux-x64/. out/.rpm/BUILD/usr/share/genet
-mv out/.rpm/BUILD/usr/share/genet/genet out/.rpm/BUILD/usr/share/genet/genet
-chrpath -r /usr/share/genet out/.rpm/BUILD/usr/share/genet/genet
-rpmbuild --define "_topdir $PWD/out/.rpm" -bb out/.rpm/genet.rpm.spec
-mv out/.rpm/RPMS/*/*.rpm out/genet-linux-amd64.rpm
+mkdir -p out/src/.rpm
+(cd out/src/.rpm && mkdir -p SOURCES BUILD RPMS SRPMS)
+sed -e "s/{{GENET_VERSION}}/$GENET_VER/g" genet.rpm.spec > out/src/.rpm/genet.rpm.spec
+cp -r debian/usr out/src/.rpm/BUILD
+mkdir -p out/src/.rpm/BUILD/usr/share/icons/hicolor/256x256/apps
+cp images/genet.png out/src/.rpm/BUILD/usr/share/icons/hicolor/256x256/apps
+cp -r out/dist/genet-linux-x64/. out/src/.rpm/BUILD/usr/share/genet
+mv out/src/.rpm/BUILD/usr/share/genet/genet out/src/.rpm/BUILD/usr/share/genet/genet
+chrpath -r /usr/share/genet out/src/.rpm/BUILD/usr/share/genet/genet
+rpmbuild --define "_topdir $PWD/out/src/.rpm" -bb out/src/.rpm/genet.rpm.spec
+mv out/src/.rpm/RPMS/*/*.rpm out/genet-linux-amd64.rpm
