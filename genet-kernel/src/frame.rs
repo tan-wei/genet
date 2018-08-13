1115208f1fff0c825a952709ab61d242061597c3
Ron Hashimoto <mail@h2so5.net>
2018-08-13 23:55:18 +0900

    genet-kernel: remove WorkerMode

--- genet-kernel/src/frame.rs
+++ genet-kernel/src/frame.rs
@@ -1,29 +1,10 @@
 use genet_abi::{attr::Attr, fixed::MutFixed, layer::Layer, token::Token};
 use std::fmt;
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum WorkerMode {
-    None,
-    Parallel(u8),
-    Serial,
-}
-
-impl WorkerMode {
-    pub fn add(&self, rhs: &WorkerMode) -> WorkerMode {
-        match (self, rhs) {
-            (WorkerMode::None, rhs) => rhs.clone(),
-            (lhs, WorkerMode::None) => lhs.clone(),
-            (lhs, rhs) if lhs == rhs => rhs.clone(),
-            _ => WorkerMode::Serial,
-        }
-    }
-}
-
 pub struct Frame {
     index: u32,
     layers: Vec<MutFixed<Layer>>,
     tree_indices: Vec<u8>,
-    worker: WorkerMode,
 }
 
 impl fmt::Debug for Frame {
@@ -40,7 +21,6 @@ impl Frame {
             index,
             layers: vec![root],
             tree_indices: Vec::new(),
-            worker: WorkerMode::None,
         }
     }
 
@@ -75,13 +55,5 @@ impl Frame {
 
     pub fn append_tree_indices(&mut self, tree_indices: &mut Vec<u8>) {
         self.tree_indices.append(tree_indices);
-    }
-
-    pub fn worker(&self) -> &WorkerMode {
-        &self.worker
-    }
-
-    pub fn set_worker(&mut self, mode: WorkerMode) {
-        self.worker = mode;
     }
 }
