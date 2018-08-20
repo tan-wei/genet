672cb8d90797632d853762d570b4adfd93a4da10
Ron Hashimoto <mail@h2so5.net>
2018-08-20 22:07:24 +0900

    license.json.gz: use accurate package names and filter out devDependencies

--- scripts/generate-license-json.js
+++ scripts/generate-license-json.js
@@ -2,24 +2,39 @@
 
 const path = require('path')
 const glob = require('glob')
-const fs = require('fs')
+const fs = require('fs-extra')
 const zlib = require('zlib')
+const { promisify } = require('util')
+const promiseGlob = promisify(glob)
 
-let files = []
-for (const dir of ['node_modules', 'plugkit']) {
-  files = files.concat(glob.sync(path.join(dir, '**/LICENSE*')))
-}
+let files = promiseGlob(path.join('node_modules/**/LICENSE*'))
 
-const contents = files.map((file) => {
-  const name = path.basename(path.dirname(file))
-  return {
-    name,
-    body: fs.readFileSync(file, 'utf8')
-  }
+const { dependencies } = fs.readJsonSync('package-lock.json')
+
+const contents = files.then((files) => {
+  return Promise.all(files.map(async (file) => {
+    try {
+      const pkg = await fs.readJson(path.join(path.dirname(file), 'package.json'))
+      const dep = dependencies[pkg.name]
+      if (!dep || dep.dev) {
+        return null
+      }
+      const body = await fs.readFile(file, 'utf8')
+      return {
+        name: pkg.name,
+        body,
+      }
+    } catch (err) {
+      return null
+    }
+  }))
 })
 
-const json = JSON.stringify(contents)
-const out = fs.createWriteStream(`genet_modules/src/asset/license.json.gz`)
-const gzip = zlib.createGzip({ level: zlib.constants.Z_BEST_COMPRESSION })
-gzip.pipe(out)
-gzip.end(json, 'utf8')
+contents.then((data) => {
+
+  const json = JSON.stringify(data.filter((item) => item))
+  const out = fs.createWriteStream('genet_modules/src/asset/license.json.gz')
+  const gzip = zlib.createGzip({ level: zlib.constants.Z_BEST_COMPRESSION })
+  gzip.pipe(out)
+  gzip.end(json, 'utf8')
+})
