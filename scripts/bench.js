91f50b691e4004bcd54417e63752ba427cbb8df6
Ron Hashimoto <mail@h2so5.net>
2018-09-09 20:40:07 +0900

    use latest electron

--- scripts/bench.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const { negatronBin } = require('./negatron')
-const runAsNode = require('./run-as-node')
-const glob = require('glob')
-
-for (const file of process.argv.slice(2)) {
-  runAsNode(file)
-}
--- scripts/negatron.js
+++ scripts/negatron.js
@@ -2,8 +2,8 @@ const fs = require('fs-extra')
 const path = require('path')
 module.exports = {
   negatronVersion: fs.readJsonSync(
-    path.join(__dirname, '../package.json')).devDependencies['@genet/negatron'],
+    path.join(__dirname, '../package.json')).devDependencies['electron'],
   negatronBin: (process.platform === 'win32')
-    ? 'node_modules\\.bin\\negatron.cmd'
-    : 'node_modules/.bin/negatron',
+    ? 'node_modules\\.bin\\electron.cmd'
+    : 'node_modules/.bin/electron',
 }
