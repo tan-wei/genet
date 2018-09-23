186bd97691925e4ee669c75084dad5aeb40887e9
Ron Hashimoto <mail@h2so5.net>
2018-09-23 22:27:05 +0900

    frames

--- genet-kernel/src/binding/session.rs
+++ genet-kernel/src/binding/session.rs
@@ -217,7 +217,7 @@ impl SessionCallback {
         let events = ev.clone();
         SessionCallback {
             asyn: Arc::new(uv::Async::new(move || {
-                let scope = HandleScope::new(env);
+                let _scope = HandleScope::new(env);
                 if let Some(json) = ev.lock().unwrap().pop() {
                     let argv = vec![env.create_string(&json).unwrap()];
                     let _ = env.call_function(&callback, &callback, &argv);
@@ -264,7 +264,9 @@ pub fn init(env: &mut Env, exports: &mut Value) -> Result<()> {
     fn profile_load_library<'env>(env: &'env Env, info: &'env CallbackInfo) -> Result<&'env Value> {
         let profile = env.unwrap::<Profile>(info.this())?;
         if let Some(value) = info.argv().get(0) {
-            profile.load_library(&env.get_value_string(value)?);
+            if let Err(err) = profile.load_library(&env.get_value_string(value)?) {
+                env.throw_error("load_library", &err.to_string())?;
+            }
             env.get_null()
         } else {
             Err(Status::InvalidArg)
@@ -281,7 +283,145 @@ pub fn init(env: &mut Env, exports: &mut Value) -> Result<()> {
         }
     }
 
-    let session_class = env.define_class("Session", session_ctor, &vec![])?;
+    fn session_frames<'env>(env: &'env Env, info: &'env CallbackInfo) -> Result<&'env Value> {
+        let session = env.unwrap::<Session>(info.this())?;
+        if let Some([start, end]) = info.argv().get(0..2) {
+            env.get_null()
+        } else {
+            Err(Status::InvalidArg)
+        }
+    }
+
+    fn session_filtered_frames<'env>(
+        env: &'env Env,
+        info: &'env CallbackInfo,
+    ) -> Result<&'env Value> {
+        let session = env.unwrap::<Session>(info.this())?;
+        if let Some([id, start, end]) = info.argv().get(0..3) {
+            env.get_null()
+        } else {
+            Err(Status::InvalidArg)
+        }
+    }
+
+    fn session_set_filter<'env>(env: &'env Env, info: &'env CallbackInfo) -> Result<&'env Value> {
+        let session = env.unwrap::<Session>(info.this())?;
+        if let Some([id, filter]) = info.argv().get(0..2) {
+            let filter = env.get_value_string(filter)?;
+            session.set_filter(
+                env.get_value_uint32(id)?,
+                if filter.is_empty() {
+                    None
+                } else {
+                    Filter::compile(&filter).ok()
+                },
+            );
+            env.get_null()
+        } else {
+            Err(Status::InvalidArg)
+        }
+    }
+
+    fn session_create_reader<'env>(
+        env: &'env Env,
+        info: &'env CallbackInfo,
+    ) -> Result<&'env Value> {
+        let session = env.unwrap::<Session>(info.this())?;
+        if let Some([id, arg]) = info.argv().get(0..2) {
+            session.create_reader(&env.get_value_string(id)?, &env.get_value_string(arg)?);
+            env.get_null()
+        } else {
+            Err(Status::InvalidArg)
+        }
+    }
+
+    fn session_create_writer<'env>(
+        env: &'env Env,
+        info: &'env CallbackInfo,
+    ) -> Result<&'env Value> {
+        let session = env.unwrap::<Session>(info.this())?;
+        if let Some([id, arg, filter]) = info.argv().get(0..3) {
+            let filter = env.get_value_string(filter)?;
+            session.create_writer(
+                &env.get_value_string(id)?,
+                &env.get_value_string(arg)?,
+                if filter.is_empty() {
+                    None
+                } else {
+                    Filter::compile(&filter).ok()
+                },
+            );
+            env.get_null()
+        } else {
+            Err(Status::InvalidArg)
+        }
+    }
+
+    fn session_close_reader<'env>(env: &'env Env, info: &'env CallbackInfo) -> Result<&'env Value> {
+        let session = env.unwrap::<Session>(info.this())?;
+        if let Some(value) = info.argv().get(0) {
+            session.close_reader(env.get_value_uint32(value)?);
+            env.get_null()
+        } else {
+            Err(Status::InvalidArg)
+        }
+    }
+
+    fn session_length<'env>(env: &'env Env, info: &'env CallbackInfo) -> Result<&'env Value> {
+        let session = env.unwrap::<Session>(info.this())?;
+        env.create_uint32(session.len() as u32)
+    }
+
+    let session_class = env.define_class(
+        "Session",
+        session_ctor,
+        &vec![
+            PropertyDescriptor::new_method(
+                env,
+                "frames",
+                PropertyAttributes::Default,
+                session_frames,
+            ),
+            PropertyDescriptor::new_method(
+                env,
+                "filteredFrames",
+                PropertyAttributes::Default,
+                session_filtered_frames,
+            ),
+            PropertyDescriptor::new_method(
+                env,
+                "setFilter",
+                PropertyAttributes::Default,
+                session_set_filter,
+            ),
+            PropertyDescriptor::new_method(
+                env,
+                "createReader",
+                PropertyAttributes::Default,
+                session_create_reader,
+            ),
+            PropertyDescriptor::new_method(
+                env,
+                "createWriter",
+                PropertyAttributes::Default,
+                session_create_writer,
+            ),
+            PropertyDescriptor::new_method(
+                env,
+                "closeReader",
+                PropertyAttributes::Default,
+                session_close_reader,
+            ),
+            PropertyDescriptor::new_property(
+                env,
+                "length",
+                PropertyAttributes::Default,
+                session_length,
+                true,
+            ),
+        ],
+    )?;
+
     let profile_class = env.define_class(
         "Profile",
         profile_ctor,
