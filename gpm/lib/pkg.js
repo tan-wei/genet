06caffda2bceed082e9518729947884c54a3c43d
Ron Hashimoto <mail@h2so5.net>
2018-10-13 15:54:35 +0900

    gpm: abi check

--- gpm/lib/pkg.js
+++ gpm/lib/pkg.js
@@ -1,9 +1,23 @@
 const crypto = require('crypto')
+const glob = require('glob')
+const path = require('path')
+const semver = require('semver')
+const getAbiVersion = require('./abi')
 class Package {
   constructor (dir, pkgJson, source) {
     Reflect.set(this, 'metadata', pkgJson)
     Reflect.set(this, 'source', source)
     Reflect.set(this, 'dir', dir)
+
+    const abiVers = glob.sync(
+      path.join(dir, '/target/release/*.{dll,so,dylib}'), { absolute: true })
+      .map((file) => getAbiVersion(file))
+    const sorted = semver.sort(abiVers.map((ver) => `${ver}.0`))
+      .map((ver) => ver.split('.').slice(0, 2)
+        .join('.'))
+    if (sorted.length > 0) {
+      Reflect.set(this, 'abi', sorted[0])
+    }
 
     const hash = crypto.createHash('sha256')
     hash.update(JSON.stringify([source, pkgJson.name]))
