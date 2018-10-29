637a9225c3e5d7898679877baa3362c0a6a4c13a
Ron Hashimoto <mail@h2so5.net>
2018-10-30 00:23:02 +0900

    eth

--- package/eth/eth/src/lib.rs
+++ package/eth/eth/src/lib.rs
@@ -1,8 +1,49 @@
 extern crate genet_sdk;
 
-use genet_sdk::{cast, decoder::*, prelude::*};
+#[macro_use]
+extern crate genet_derive;
 
-struct EthWorker {}
+#[derive(Attr, Default)]
+/// Ethernet
+struct EthLayer {
+    /// Source Hardware Address
+    #[genet(alias = "_.src")]
+    src: MacAddr,
+
+    /// Destination Hardware Address
+    #[genet(alias = "_.dst")]
+    dst: MacAddr,
+
+    /// Protocol Type
+    #[genet(typ = "@enum")]
+    type_type: Detach<EthType>,
+}
+
+#[derive(Attr, Default)]
+struct MacAddr {
+    #[genet(typ = "@eth:mac")]
+    _self: Uint8,
+}
+
+#[derive(Attr, Default)]
+struct EthType {
+    _self: Uint8,
+
+    /// IPv4
+    ipv4: Detach<Uint8>,
+
+    /// IPv6
+    ipv6: Detach<Uint8>,
+
+    /// ARP
+    arp: Detach<Uint8>,
+}
+
+use genet_sdk::{cast, decoder::*, field::*, prelude::*};
+
+struct EthWorker {
+    eth: EthLayer,
+}
 
 impl Worker for EthWorker {
     fn decode(
@@ -43,7 +84,15 @@ struct EthDecoder {}
 
 impl Decoder for EthDecoder {
     fn new_worker(&self, _ctx: &Context) -> Box<Worker> {
-        Box::new(EthWorker {})
+        use genet_sdk::attr::{AttrContext, AttrNode};
+        let mut eth = EthLayer::default();
+        let ch = eth.init(&AttrContext {
+            path: "eth".into(),
+            ..AttrContext::default()
+        });
+        println!("{:#?}", ch);
+
+        Box::new(EthWorker { eth })
     }
 
     fn metadata(&self) -> Metadata {
