56d4d999a05ce07df5ef5156ba91f8556278e32d
Ron Hashimoto <mail@h2so5.net>
2019-01-20 00:55:10 +0900

    update fmt.sh

--- fmt.sh
+++ fmt.sh
@@ -2,7 +2,7 @@
 
 find . -name Cargo.toml|while read fname; do
   DIR=$(dirname "$fname")
-  (cd $DIR; cargo +nightly fmt)
+  (cd $DIR; cargo +nightly fmt; cargo tomlfmt)
 done
 
 (cd genet-node && clang-format -i src/*.*)
