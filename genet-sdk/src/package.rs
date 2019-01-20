09f011106cb4d03690b8e3b6c4f06550c0721cd5
Ron Hashimoto <mail@h2so5.net>
2019-01-21 00:30:19 +0900

    ByteData

--- genet-sdk/src/package.rs
+++ genet-sdk/src/package.rs
@@ -1,23 +1,61 @@
 use crate::decoder::DecoderBox;
-use serde::ser;
+use serde::{
+    de::{self, Deserialize, Visitor},
+    ser,
+};
 use serde_derive::{Deserialize, Serialize};
+use std::{fmt, mem, ptr};
 
-pub struct Copyable<T> {
+pub struct ByteData<T: Clone> {
     data: T,
 }
 
-impl<T> Copyable<T> {
+impl<T: Clone> ByteData<T> {
     unsafe fn new(data: T) -> Self {
         Self { data }
     }
+
+    pub fn unwrap(self) -> T {
+        self.data
+    }
 }
 
-impl<T> ser::Serialize for Copyable<T> {
+impl<T: Clone> ser::Serialize for ByteData<T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: ser::Serializer,
+        T: Sized,
     {
-        serializer.serialize_i32(0)
+        let mut buf = vec![0; mem::size_of::<T>()];
+        unsafe { ptr::write(buf.as_mut_ptr() as *mut T, self.data.clone()) };
+        serializer.serialize_bytes(&buf)
+    }
+}
+
+struct BufVisitor;
+
+impl<'de> Visitor<'de> for BufVisitor {
+    type Value = &'de [u8];
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a byte slice")
+    }
+
+    fn visit_borrowed_bytes<E>(self, value: &'de [u8]) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        Ok(value)
+    }
+}
+
+impl<'de, T: Clone> Deserialize<'de> for ByteData<T> {
+    fn deserialize<D>(deserializer: D) -> Result<ByteData<T>, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        let buf = deserializer.deserialize_bytes(BufVisitor)?;
+        unsafe { Ok(ByteData::new(ptr::read(mem::transmute(buf.as_ptr())))) }
     }
 }
 
@@ -33,7 +71,7 @@ pub enum Component {
     Decoder {
         id: String,
         trigger_after: Vec<String>,
-        //decoder: Copyable<DecoderBox>,
+        decoder: ByteData<DecoderBox>,
     },
     Reader {
         id: String,
