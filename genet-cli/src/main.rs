59dde630b7094938e003bd715ebfc5094829df40
Ron Hashimoto <mail@h2so5.net>
2019-01-27 22:50:01 +0900

    grid

--- genet-cli/src/main.rs
+++ genet-cli/src/main.rs
@@ -1,6 +1,7 @@
 use eth;
 use genet_kernel::profile::Profile;
 use genet_sdk::package::Package;
+use term_grid::{Direction, Filling, Grid, GridOptions};
 
 fn main() {
     let packages: Vec<Package> = vec![include!(concat!(env!("OUT_DIR"), "/builtin_packages.rs"))];
@@ -8,5 +9,11 @@ fn main() {
     for pkg in packages.into_iter() {
         prof.register_package(pkg);
     }
-    println!("Hello, world!");
+
+    let grid = Grid::new(GridOptions {
+        filling: Filling::Spaces(1),
+        direction: Direction::LeftToRight,
+    });
+
+    println!("{}", grid.fit_into_width(64).unwrap());
 }
