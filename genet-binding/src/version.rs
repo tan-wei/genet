d45df53c9fce8fd3f787d52bcfcebf9186d3beb4
Ron Hashimoto <mail@h2so5.net>
2019-01-30 23:26:03 +0900

    genet-binding: fix missing mod

--- genet-binding/src/version.rs
+++ genet-binding/src/version.rs
@@ -1,9 +1,9 @@
-use genet_abi::env;
+use genet_abi::genet_abi_version;
 use genet_napi::napi::{Env, Result, Value};
 
 pub fn init(env: &Env, exports: &Value) -> Result<()> {
     let tk = env.create_object()?;
-    let abi = env::genet_abi_version();
+    let abi = genet_abi_version();
     let abi = format!("{}.{}", abi >> 32, abi & 0xffff_ffff);
     env.set_named_property(tk, "abi", env.create_string(&abi)?)?;
     env.set_named_property(exports, "version", tk)?;
