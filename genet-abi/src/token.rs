343bf0e4a75e31ed48596ea964f9f0be1984ced2
Ron Hashimoto <mail@h2so5.net>
2019-02-03 03:02:27 +0900

    token

--- genet-abi/src/token.rs
+++ genet-abi/src/token.rs
@@ -51,74 +51,16 @@ impl<'a> From<&'a str> for Token {
     }
 }
 
-#[repr(C)]
-pub struct TokenRegistry {
-    token: unsafe extern "C" fn(*const u8, u64) -> Token,
-    string: unsafe extern "C" fn(Token, *mut u64) -> *const u8,
-}
-
-impl Default for TokenRegistry {
-    fn default() -> Self {
-        Self {
-            token: abi_genet_get_token,
-            string: abi_genet_get_string,
-        }
-    }
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn genet_abi_v1_register_token_registry(registry: TokenRegistry) {
-    GENET_GET_TOKEN = registry.token;
-    GENET_GET_STRING = registry.string;
-}
-
-static mut GENET_GET_TOKEN: unsafe extern "C" fn(*const u8, u64) -> Token = abi_genet_get_token;
-static mut GENET_GET_STRING: unsafe extern "C" fn(Token, *mut u64) -> *const u8 =
-    abi_genet_get_string;
-
-unsafe extern "C" fn abi_genet_get_token(data: *const u8, len: u64) -> Token {
-    let tokens = GLOBAL_TOKENS.lock();
-    let mut tokens = tokens.borrow_mut();
-    let strings = GLOBAL_STRINGS.lock();
-    let mut strings = strings.borrow_mut();
-    let id = str::from_utf8_unchecked(slice::from_raw_parts(data, len as usize));
-    if id.is_empty() {
-        return Token::null();
-    }
-    let next = tokens.len() + 1;
-    let entry = tokens.entry(String::from(id));
-    if let Entry::Vacant(_) = entry {
-        strings.push(String::from(id));
-    }
-    *entry.or_insert_with(|| Token::from(next as u128))
-}
-
-unsafe extern "C" fn abi_genet_get_string(token: Token, len: *mut u64) -> *const u8 {
-    let strings = GLOBAL_STRINGS.lock();
-    let strings = strings.borrow();
-    let index: u128 = token.into();
-    let index = index as usize;
-    let s = if index < strings.len() {
-        strings[index].as_str()
-    } else {
-        ""
-    };
-    *len = s.len() as u64;
-    s.as_ptr()
-}
-
-lazy_static! {
-    static ref GLOBAL_TOKENS: Mutex<RefCell<FnvHashMap<String, Token>>> =
-        Mutex::new(RefCell::new(FnvHashMap::default()));
-    static ref GLOBAL_STRINGS: Mutex<RefCell<Vec<String>>> =
-        Mutex::new(RefCell::new(vec![String::new()]));
-}
-
 fn token(id: &str) -> Token {
     if id.is_empty() {
         Token::null()
     } else {
-        unsafe { GENET_GET_TOKEN(id.as_ptr(), id.len() as u64) }
+        let mut hash = 0x6c62272e07bb014262b821756295c58d;
+        for byte in id.as_bytes() {
+            hash = hash ^ (*byte as u128);
+            hash = hash.wrapping_mul(0x1000000000000000000013b);
+        }
+        hash.into()
     }
 }
 
