556e3c657e1b75bcefa075d787925a9e03168378
Ron Hashimoto <mail@h2so5.net>
2019-02-16 20:21:06 +0900

    variant

--- genet-chamber/src/variant.rs
+++ genet-chamber/src/variant.rs
@@ -1,1 +1,9 @@
-pub enum Variant {}
+#[derive(Debug, Clone, PartialEq)]
+pub enum Variant {
+    Nil,
+    Bool(bool),
+    Int64(i64),
+    UInt64(u64),
+    Float64(f64),
+    Buffer(Box<[u8]>),
+}
