6dd769082ca0f4bba2f301447f96893da0d77b28
Ron Hashimoto <mail@h2so5.net>
2019-02-16 21:28:13 +0900

    filter

--- genet-chamber/src/chamber.rs
+++ genet-chamber/src/chamber.rs
@@ -1,7 +1,7 @@
-use crate::token::Token;
+use crate::{filter::CompiledFilter, token::Token};
 use fnv::FnvHashMap;
 use parking_lot::Mutex;
-use std::{slice, str, sync::Arc};
+use std::{ops::Range, slice, str, sync::Arc};
 
 pub struct Chamber {
     inner: Inner,
@@ -29,6 +29,10 @@ pub struct Context {
 impl Context {
     pub(crate) fn get_token(&self, s: &str) -> Token {
         (self.get_token)(self.inner, s.as_ptr(), s.len() as u64)
+    }
+
+    pub fn filter(&self, filter: &CompiledFilter, range: Range<usize>) -> Vec<usize> {
+        vec![]
     }
 }
 
--- genet-chamber/src/lib.rs
+++ genet-chamber/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod attr;
 pub mod chamber;
 pub mod field;
+pub mod filter;
 pub mod frame;
 pub mod result;
 pub mod token;
